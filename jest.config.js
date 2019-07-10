95f400b162aefca7a9097966daa186d484a4e482
Javier Diaz <hello@coderdiaz.me>
2019-07-10 11:03:03 -0500

    fix: code linter errors

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,19 @@ module.exports = {
   "globals": {
     "ts-jest": {
       "diagnostics": {
-        "ignoreCodes": [151001]
-      }
-    }
+        "ignoreCodes": [151001],
+      },
+    },
   },
   "roots": [
-    "<rootDir>/tests/unit"
+    "<rootDir>/tests/unit",
   ],
   "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest",
   },
   "snapshotSerializers": ["enzyme-to-json/serializer"],
   "setupFilesAfterEnv": ["<rootDir>/tests/setupEnzyme.ts"],
   "transformIgnorePatterns": [
     '/node_modules/',
   ],
-}
+};
