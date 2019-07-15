5fb44a2c50d58fc2e09e6c3644f19d84df46a496
JoseLAmador <josel.amador.s@gmail.com>
2019-07-15 16:58:52 -0500

    fix: minor fixes

--- docs/wrapper.js
+++ docs/wrapper.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
-import '../packages/css/tailwind.css'
+import '../packages/css/tailwind.css';
 
-const Wrapper = ({ children }) => <Fragment> {children} </Fragment>
+const Wrapper = ({ children }) => <Fragment> {children} </Fragment>;
 
 export default Wrapper;
