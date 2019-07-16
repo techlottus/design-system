9145e454c47f9766417cf32c42e5f71d72493111
Yonatan Rojas S <yonatan@bedu.org>
2019-07-16 13:40:12 -0500

    feat: added font awesome icons for test

--- doczrc.js
+++ doczrc.js
@@ -12,4 +12,11 @@ export default {
     })
   ],
   wrapper: 'docs/wrapper',
+  htmlContext: {
+    head: {
+      links: [{
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css'
+      }]
+  }}
 };
