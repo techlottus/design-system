5112610e74185328b3ac5737563154664ad45b68
Yonatan Rojas S <yonatan@bedu.org>
2019-07-17 17:58:13 -0500

    fix: refactor IconProps to match design requirements

--- packages/Icon/index.tsx
+++ packages/Icon/index.tsx
@@ -2,20 +2,24 @@ import * as React from 'react';
 
 // Icon Props
 interface IconProps {
-  size?: string;
+  size?: '12px'|'14px'|'16px'|'18px'|'24px'|'32px';
+  state?: 'text-neutral'|'text-active'|'text-warning'|'text-alert'|'text-disabled';
   className?: string;
+  icon: string;
 }
 
-const Icon: React.SFC<IconProps> = ({ size, className }: IconProps) => {
-  return(
+const Icon: React.SFC<IconProps> = ({ size, state, className, icon }: IconProps) => {
+  return (
     <div>
-      <i style={{ fontSize: size }} className={className}></i>
+      <i style={{ fontSize: size }} className={`${className} ${state} ${icon}`}></i>
     </div>
   );
 };
 
 Icon.defaultProps = {
   size: '12px',
+  state: 'text-neutral',
+  className: '',
 };
 
 export default Icon;
