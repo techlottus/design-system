9587b0fb899dafc9d4b0283155a83508e421e709
Yonatan Rojas S <yonatan@bedu.org>
2019-08-11 19:40:12 -0500

    feat: added variantNames and variantIntensity

--- packages/ProgressBar/index.tsx
+++ packages/ProgressBar/index.tsx
@@ -2,14 +2,46 @@ import * as React from 'react';
 
 interface ProgressBarProps {
   percentage: number;
+  variant?: string;
+  variantAs?: string;
 }
 
-const ProgressBar: React.SFC<ProgressBarProps> = ({percentage}: ProgressBarProps) => {
+const variantName = {
+  surface: 'surface',
+  primary: 'primary',
+  secondary: 'secondary',
+  technology: 'technology',
+  english: 'english',
+  business: 'business',
+  prepa: 'prepa',
+};
+
+const variantIntensity = {
+  50: '50',
+  100: '100',
+  200: '200',
+  300: '300',
+  400: '400',
+  500: '500',
+  600: '600',
+  700: '700',
+  800: '800',
+  900: '900',
+  950: '950',
+};
+
+const ProgressBar: React.SFC<ProgressBarProps> = ({percentage, variant, variantAs}: ProgressBarProps) => {
   return (
-    <div className='w-full bg-surface-100 rounded'>
-      <span className='h-full bg-surface-500' style={{width: '25%'}}></span>
+    <div className='w-full h-2 bg-surface-100 rounded'>
+      <div className={`h-full rounded bg-${variantName[variant]}-${variantIntensity[variantAs]}`} style={{width: `${percentage}%`}}></div>
     </div>
   );
+};
+
+ProgressBar.defaultProps = {
+  percentage: 0,
+  variant: 'primary',
+  variantAs: '200',
 };
 
 export default ProgressBar;
