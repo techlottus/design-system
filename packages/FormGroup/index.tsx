4359f297f51d9b3783e2e533a3ab86fe161d7efd
JoseLAmador <josel.amador.s@gmail.com>
2019-08-17 00:05:13 -0500

    feature: add input components

--- packages/FormGroup/index.tsx
+++ packages/FormGroup/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
-interface FormGroupProps {
+interface IFormGroupProps {
   children?: React.ReactNode;
   className?: string;
 }
 
-const FormGroup: React.SFC<FormGroupProps> = ({ children, className= 'p-5' }: FormGroupProps) => (
+const FormGroup: React.SFC<IFormGroupProps> = ({ children, className }: IFormGroupProps) => (
   <div className={`${className}`}>{children}</div>
 );
+
+FormGroup.defaultProps = {
+  className: 'p-5',
+};
 
 export default FormGroup;
