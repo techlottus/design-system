1df1c6f092176845dafbf4383d37056539534946
Yonatan Rojas S <yonatan@bedu.org>
2019-08-18 13:37:32 -0500

    refactor: refactor of Button component

--- packages/Button/index.tsx
+++ packages/Button/index.tsx
@@ -1,85 +1,36 @@
 import * as React from 'react';
+import { IProps } from '../common/props';
+import style from './button.module.pcss';
 
 // Button Props
-const sizes = {
-  xs: '',
-  sm: '',
-  md: 'px-8 py-3 text-base',
-  lg: 'px-10 py-5 text-xl',
+const Sizes = {
+  md: style.btnMedium,
+  lg: style.btnLarge,
 };
 
-const buttonVariant = {
-  primary: `
-    bg-primary-400
-    hover:bg-primary-700
-    rounded
-    focus:outline-none
-    focus:shadow-primary-200
-    focus:bg-primary-700
-    inline-flex
-    justify-center
-    items-center
-    text-white
-    font-bold
-    font-montserrat
-  `,
-  secondary: `
-    bg-surface-500
-    hover:bg-surface-800
-    rounded
-    focus:outline-none
-    focus:shadow-secondary-200
-    focus:bg-surface-800
-    inline-flex
-    justify-center
-    items-center
-    text-white
-    font-bold
-    font-montserrat
-  `,
-  terciary: `
-    bg-transparent
-    hover:bg-surface-400
-    hover:text-white
-    focus:outline-none
-    focus:shadow-secondary-200
-    focus:bg-surface-400
-    focus:text-white
-    border-2
-    border-surface-400
-    rounded
-    inline-flex
-    justify-center
-    items-center
-    font-bold
-    font-montserrat
-  `,
-  disabled: `
-    bg-surface-100
-    focus:outline-none
-    rounded
-    inline-flex
-    justify-center
-    items-center
-    text-surface-300
-    font-bold
-    font-montserrat
-    cursor-not-allowed
-  `,
+type Size = typeof Sizes[keyof typeof Sizes];
+
+const Variants = {
+  primary: style.btnPrimary,
+  secondary: style.btnSecondary,
+  terciary: style.btnTerciary,
+  disabled: style.btnDisabled,
 };
 
-interface IButton {
-  size: 'xs'|'sm'|'md'|'lg';
-  variant: 'primary'|'secondary'|'terciary'|'disabled';
-  children: any;
-  className?: string;
+type Variant = typeof Variants[keyof typeof Variants];
+
+export interface IButton extends IProps {
+  size: Size;
+  variant: Variant;
+  children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }
 
-const Button: React.SFC<IButton> = ({ size, variant, children, className, onClick }: IButton) => (
+const Button: React.SFC<IButton> = ({ children, onClick, size, variant, className }: IButton) => (
   <button
     onClick={onClick}
-    className={`${className} ${sizes[size]} ${buttonVariant[variant]}`}>
+    className={`${Sizes[size]} ${Variants[variant]} ${className}`}>
     {children}
   </button>
 );
@@ -87,7 +38,6 @@ const Button: React.SFC<IButton> = ({ size, variant, children, className, onClic
 Button.defaultProps = {
   size: 'md',
   variant: 'primary',
-  className: '',
 };
 
 export default Button;
