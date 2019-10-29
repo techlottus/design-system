049716ff4ad16549da4f16ac2d45c1bff6512bb8
Javier Diaz Chamorro <hello@coderdiaz.me>
2019-10-29 15:19:51 -0600

    feat: added component for use only with form group

--- packages/Forms/FormGroup.tsx
+++ packages/Forms/FormGroup.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IProps } from '../common/props';
 import classNames from 'classnames';
 import styles from './form-group.module.pcss';
+import Message from './Message';
 
 interface IFormGroupProps extends IProps {
   children?: React.ReactNode;
@@ -10,24 +11,28 @@ interface IFormGroupProps extends IProps {
   labelFor?: string;
 }
 
-const FormGroup: React.FunctionComponent<IFormGroupProps> = ({ children, className, helperText, label, labelFor }: IFormGroupProps) => {
-  const classes = classNames(
-    'form-group',
-    styles.formGroup,
-    className,
-  );
+class FormGroup extends React.PureComponent<IFormGroupProps, {}> {
+  public static Message = Message;
+  render() {
+    const { children, className, helperText, label, labelFor } = this.props;
+    const classes = classNames(
+      'form-group',
+      styles.formGroup,
+      className,
+    );
 
-  return (
-    <div className={classes}>
-      { label && <label className={styles.formGroupLabel} htmlFor={labelFor}>
-        {label}
-      </label>}
-      <div className={styles.formGroupContent}>
-        {children}
-        { helperText && <div className={styles.formGroupHelperText}>{helperText}</div>}
+    return (
+      <div className={classes}>
+        { label && <label className={styles.formGroupLabel} htmlFor={labelFor}>
+          {label}
+        </label>}
+        <div className={styles.formGroupContent}>
+          {children}
+          { helperText && <div className={styles.formGroupHelperText}>{helperText}</div>}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FormGroup;
