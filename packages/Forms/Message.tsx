7940bda201c37469cc84febed7e0db76eccdc834
Javier Diaz Chamorro <hello@coderdiaz.me>
2019-10-29 15:19:27 -0600

    feat: added message component for validation msg

---
+++ packages/Forms/Message.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { IProps } from '../common/props';
+import classNames from 'classnames';
+
+export interface IMessageProps extends IProps {
+  children?: React.ReactNode;
+  show?: boolean;
+  type: 'default'| 'error' | 'success' | 'warning' | 'info';
+}
+
+const defaultClasses = classNames('px-2', 'py-1', 'rounded');
+const messageColorTypes = {
+  error: classNames('bg-primary-100', 'text-primary-alert', defaultClasses),
+  warning: classNames('bg-warning', defaultClasses),
+  success: classNames('bg-success', defaultClasses),
+  default: classNames('bg-surface-100', defaultClasses),
+};
+
+class Message extends React.PureComponent<IMessageProps, {}> {
+  public static defaultProps: IMessageProps = {
+    show: true,
+    type: 'default',
+  };
+
+  render() {
+    const { type, children, className } = this.props;
+    const classes = classNames(
+      'box__message',
+      className,
+      messageColorTypes[type],
+    );
+    return (
+      <div className={classes}>
+        {children}
+      </div>
+    );
+  }
+}
+
+export default Message;
