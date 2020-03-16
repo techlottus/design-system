ba4e1605bfa07be90b94b9bd009828e60aa11679
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-16 00:39:29 -0600

    feat: updated avatar component

--- packages/Avatar/index.tsx
+++ packages/Avatar/index.tsx
@@ -1,34 +1,30 @@
 import * as React from 'react';
 import { IProps } from '../common/props';
 import classNames from 'classnames';
-import style from './avatar.module.pcss';
 
 const Sizes = {
-  'xs': style.avatarExtraSmall,
-  'sm': style.avatarSmall,
-  'md': style.avatarMedium,
-  'lg': style.avatarLarge,
-  'xl': style.avatarExtraLarge,
-  '2xl': style.avatarDoubleExtraLarge,
+  'xs': classNames('w-4 h-4'),
+  'sm': classNames('w-6 h-6'),
+  'md': classNames('w-8 h-8'),
+  'lg': classNames('w-10 h-10'),
+  'xl': classNames('w-12 h-12'),
+  '2xl': classNames('w-14 h-14'),
 };
 
 type Size = typeof Sizes[keyof typeof Sizes];
 
 export interface IAvatar extends IProps {
   children?: React.ReactNode;
+  src?: string;
   size?: Size;
 }
 
-const Avatar: React.SFC<IAvatar> = ({ size, children, className }: IAvatar) => {
-  const classes = classNames(
-    style.avatar,
-    className,
-    Sizes[size],
-  );
+const Avatar: React.SFC<IAvatar> = ({ size, src, className }: IAvatar) => {
+  const classes = classNames('relative', className, Sizes[size]);
 
   return (
     <figure className={classes}>
-      {children}
+      <img src={src} className='w-full h-full relative rounded' />
     </figure>
   );
 };
