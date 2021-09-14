23390857e4d423d7edfdd72488ee67cebefc11f1
JoseLAmador <josel.amador.s@gmail.com>
2021-09-14 15:24:39 -0500

    fix: modify next config

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -4,4 +4,24 @@ const withNextra = require('nextra')({
   unstable_staticImage: true,
 });
 
-module.exports = withNextra;
+const nextConfig = {
+  webpack: function (config, { defaultLoaders }) {
+    const resolvedBaseUrl = path.resolve(config.context, "../../");
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(tsx|ts|js|mjs|jsx)$/,
+        include: [resolvedBaseUrl],
+        use: defaultLoaders.babel,
+        exclude: (excludePath) => {
+          return /node_modules/.test(excludePath);
+        },
+      },
+    ];
+    return config;
+  },
+};
+
+module.exports = withNextra({
+  ...nextConfig
+});
