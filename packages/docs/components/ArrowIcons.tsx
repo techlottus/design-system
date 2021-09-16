d09fee069c7e5d42085d6441efa2104419021053
Javier <coderdiaz@gmail.com>
2021-09-15 22:01:57 -0500

    feat: added documentation for iconography

---
+++ packages/docs/components/ArrowIcons.tsx
@@ -0,0 +1,129 @@
+import {
+  HybridIcon,
+  SyncIcon,
+  ExpandIcon,
+  ContractIcon,
+  CaretDownFillIcon,
+  CaretLeftFillIcon,
+  CaretRightFillIcon,
+  CaretUpFillIcon,
+  CaretLeftIcon,
+  CaretRightIcon,
+  CaretDownIcon,
+  CaretUpIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  BottomDrawerIcon,
+  PublishIcon,
+  DownloadIcon,
+  FirstPageIcon,
+  LastPageIcon,
+  RepeatIcon,
+  LogOutIcon,
+  ExitIcon,
+} from "@exponentialeducation/iconography/icons/esm";
+
+const ArrowIcons = () => {
+  return <div className="mt-10 grid grid-cols-6 gap-10">
+    <div className="flex flex-col items-center space-y-2">
+      <HybridIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">hybrid</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <SyncIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">sync</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ExpandIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">expand</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ContractIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">contract</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretDownFillIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-down-fill</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretLeftFillIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-left-fill</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretRightFillIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-right-fill</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretUpFillIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-up-fill</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretLeftIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-left</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretRightIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-right</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretDownIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-down</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <CaretUpIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">caret-up</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ArrowDownIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">arrow-down</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ArrowUpIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">arrow-up</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ArrowLeftIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">arrow-left</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ArrowRightIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">arrow-right</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <BottomDrawerIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">bottom-drawer</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <PublishIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">publish</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <DownloadIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">download</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <FirstPageIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">first-page</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <LastPageIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">last-page</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <RepeatIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">repeat</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <LogOutIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">log-out</span>
+    </div>
+    <div className="flex flex-col items-center space-y-2">
+      <ExitIcon className="w-8 h-8" />
+      <span className="text-xs text-gray-600">exit</span>
+    </div>
+  </div>
+};
+
+export default ArrowIcons;
