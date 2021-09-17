5c09f82ad5395971620b7718e0d1bb53bb7073b7
Javier <coderdiaz@gmail.com>
2021-09-17 12:12:31 -0500

    fix: fixed icon names

--- packages/docs/components/ArrowIcons.tsx
+++ packages/docs/components/ArrowIcons.tsx
@@ -29,99 +29,99 @@ const ArrowIcons = () => {
   return <div className="mt-10 grid grid-cols-6 gap-10">
     <div className="flex flex-col items-center space-y-2">
       <HybridIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">hybrid</span>
+      <span className="text-xs text-gray-600">{HybridIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <SyncIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">sync</span>
+      <span className="text-xs text-gray-600">{SyncIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ExpandIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">expand</span>
+      <span className="text-xs text-gray-600">{ExpandIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ContractIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">contract</span>
+      <span className="text-xs text-gray-600">{ContractIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretDownFillIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-down-fill</span>
+      <span className="text-xs text-gray-600">{CaretDownFillIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretLeftFillIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-left-fill</span>
+      <span className="text-xs text-gray-600">{CaretLeftFillIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretRightFillIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-right-fill</span>
+      <span className="text-xs text-gray-600">{CaretRightFillIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretUpFillIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-up-fill</span>
+      <span className="text-xs text-gray-600">{CaretUpFillIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretLeftIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-left</span>
+      <span className="text-xs text-gray-600">{CaretLeftIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretRightIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-right</span>
+      <span className="text-xs text-gray-600">{CaretRightIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretDownIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-down</span>
+      <span className="text-xs text-gray-600">{CaretDownIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <CaretUpIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">caret-up</span>
+      <span className="text-xs text-gray-600">{CaretUpIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ArrowDownIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">arrow-down</span>
+      <span className="text-xs text-gray-600">{ArrowDownIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ArrowUpIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">arrow-up</span>
+      <span className="text-xs text-gray-600">{ArrowUpIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ArrowLeftIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">arrow-left</span>
+      <span className="text-xs text-gray-600">{ArrowLeftIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ArrowRightIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">arrow-right</span>
+      <span className="text-xs text-gray-600">{ArrowRightIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <BottomDrawerIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">bottom-drawer</span>
+      <span className="text-xs text-gray-600">{BottomDrawerIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <PublishIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">publish</span>
+      <span className="text-xs text-gray-600">{PublishIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <DownloadIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">download</span>
+      <span className="text-xs text-gray-600">{DownloadIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <FirstPageIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">first-page</span>
+      <span className="text-xs text-gray-600">{FirstPageIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <LastPageIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">last-page</span>
+      <span className="text-xs text-gray-600">{LastPageIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <RepeatIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">repeat</span>
+      <span className="text-xs text-gray-600">{RepeatIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <LogOutIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">log-out</span>
+      <span className="text-xs text-gray-600">{LogOutIcon.name}</span>
     </div>
     <div className="flex flex-col items-center space-y-2">
       <ExitIcon className="w-8 h-8" />
-      <span className="text-xs text-gray-600">exit</span>
+      <span className="text-xs text-gray-600">{ExitIcon.name}</span>
     </div>
   </div>
 };
