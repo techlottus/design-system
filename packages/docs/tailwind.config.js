df967fe281458e8e11aadb72e40f41b31ad3ad49
JoseLAmador <josel.amador.s@gmail.com>
2021-09-21 12:35:37 -0500

    fix: modify purge path

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: [
       './pages/**/*.{js,ts,tsx,mdx}',
       './components/**/*.{js,ts,tsx}',
-      '../../node_modules/@exponentialeducation/betomic/src//**/*.{js,ts,tsx}'
+      '../../node_modules/@exponentialeducation/betomic/src/**/*.{js,ts,tsx}'
     ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
