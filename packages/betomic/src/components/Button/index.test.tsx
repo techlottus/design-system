6096f62906f5b2c0f2bcfeee1bfb8733138c58df
sifloz <hecn@live.com.mx>
2021-09-20 19:13:30 -0500

    feat: Tested Button component variant prop

--- packages/betomic/src/components/Button/index.test.tsx
+++ packages/betomic/src/components/Button/index.test.tsx
@@ -48,4 +48,71 @@ describe("<Button />", () => {
     ).not.toBeDisabled();
   });
 
+  // Testing button variants
+  test("Render primary button with default props set", () => {
+    render(
+      <Button variant="primary">
+        Primary button
+      </Button>
+    );
+
+    expect(
+      screen.getByRole("button")
+    ).toHaveClass("bg-primary-500 text-white hover:bg-primary-600 focus:bg-primary-600 focus:border focus:border-primary-500");
+
+    // Not secondary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-surface-600 hover:bg-surface-900 focus:bg-surface-900");
+
+    // Not tertiary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-white text-surface-600 border-surface-500 hover:bg-surface-500 hover:text-white focus:bg-surface-500");
+  });
+
+  test("Render secondary button with default props set", () => {
+    render(
+      <Button variant="secondary">
+        Secondary button
+      </Button>
+    );
+
+    expect(
+      screen.getByRole("button")
+    ).toHaveClass("bg-surface-600 text-white hover:bg-surface-900 focus:bg-surface-900 focus:border focus:border-primary-500");
+    
+    // Not primary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-primary-500 hover:bg-primary-600 focus:bg-primary-600");
+
+    // Not tertiary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-white text-surface-600 border-surface-500 hover:bg-surface-500 hover:text-white focus:bg-surface-500");
+  
+  });
+
+  test("Render tertiary button with default props set", () => {
+    render(
+      <Button variant="tertiary">
+        Tertiary button
+      </Button>
+    );
+
+    expect(
+      screen.getByRole("button")
+    ).toHaveClass("bg-white text-surface-600 border-2 border-surface-500 hover:bg-surface-500 hover:text-white focus:bg-surface-500 focus:text-white focus:border-primary-500");
+    
+    // Not primary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-primary-500 hover:bg-primary-600 focus:bg-primary-600");
+      
+    // Not secondary
+    expect(
+      screen.getByRole("button")
+    ).not.toHaveClass("bg-surface-600 hover:bg-surface-900 focus:bg-surface-900");
+  });
 });
