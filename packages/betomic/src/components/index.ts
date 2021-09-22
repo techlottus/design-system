1f20db9e8959471352243ad2039c93f888cf8244
JoseLAmador <josel.amador.s@gmail.com>
2021-09-22 17:27:41 -0500

    feat: add form group component

--- packages/betomic/src/components/index.ts
+++ packages/betomic/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as HelloWorld } from "./HelloWorld";
 export { default as Button } from "./Button";
 export { default as InputGroup } from "./InputGroup";
+export { default as FormGroup } from "./FormGroup";
