6b07ba062b18cf3f010faa245e482b4873390396
JoseLAmador <josel.amador.s@gmail.com>
2021-09-22 13:14:17 -0500

    chore: remove unnecessary input code

--- packages/betomic/src/components/index.ts
+++ packages/betomic/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as HelloWorld } from "./HelloWorld";
 export { default as Button } from "./Button";
-export { default as Input } from "./Input";
 export { default as InputGroup } from "./InputGroup";
