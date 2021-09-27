84bf4f0ff0ffabaf02693144892a5cf0d1827e18
sifloz <hecn@live.com.mx>
2021-09-27 12:30:40 -0500

    chore: Added pagination to component export list

--- packages/betomic/src/components/index.ts
+++ packages/betomic/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as HelloWorld } from "./HelloWorld";
 export { default as Button } from "./Button";
 export { default as InputGroup } from "./InputGroup";
 export { default as FormGroup } from "./FormGroup";
+export { default as Pagination } from "./Pagination";
