b3b66abbb7e661994df11a2e22a15c5d59373438
sifloz <hecn@live.com.mx>
2021-10-06 18:10:02 -0500

    feat: Added Badge to betomic components export list

--- packages/betomic/src/components/index.ts
+++ packages/betomic/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as InputGroup } from "./InputGroup";
 export { default as FormGroup } from "./FormGroup";
 export { default as Pagination } from "./Pagination";
+export { default as Badge } from "./Badge";
