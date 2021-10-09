a76f38baa4a50feabddc7cfcb3c5ce5b42ace3b2
sifloz <hecn@live.com.mx>
2021-10-08 20:32:57 -0500

    fix: Updated DateInput component

--- packages/datetime/src/DateInput/index.tsx
+++ packages/datetime/src/DateInput/index.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import MomentLocaleUtils from "react-day-picker/moment";
 import { InputGroup } from "@exponentialeducation/betomic/src";
-import {
-  CaretLeftIcon,
-  CaretRightIcon,
-  CalendarIcon
-} from "@exponentialeducation/iconography/icons";
 import {
   NavbarElementProps,
   WeekdayElementProps,
@@ -21,16 +16,17 @@ interface IDateInput {
   canChangeMonth?: boolean;
   dayPickerProps?: DayPickerProps;
   defaultValue?: Date;
+  disabled?: boolean;
   disabledDates?: Modifier | Modifier[];
-  format?: "L" | "l" | "LL" | "ll";
   id?: string;
   initialMonth?: Date;
-  locale?: string;
   maxDate?: Date;
   minDate?: Date;
   modifiers?: Partial<Modifiers>;
   onChange?: (selectedDate: Date) => void;
+  placeholder?: string;
   showOutsideDays?: boolean;
+  valid?: boolean;
 };
 
 const DateInput: React.FC<IDateInput> = (props: IDateInput) => {
@@ -40,133 +36,54 @@ const DateInput: React.FC<IDateInput> = (props: IDateInput) => {
     dayPickerProps,
     defaultValue,
     disabledDates,
-    format = "L",
     id,
     initialMonth,
-    locale = "es-mx",
     minDate,
     maxDate,
     modifiers,
     onChange = f => f,
+    placeholder = "DD/MM/YYYY",
     showOutsideDays = true,
+    valid,
+    disabled,
   } = props;
-
-  const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined);
-  const [isValidDay, setIsValidDay] = useState<boolean | null>(null);
-
-  const dateInputRef: React.Ref<HTMLInputElement> = useRef(null);
-
-  useEffect(() => {
-    if (defaultValue) {
-      setInitialSelectedDay(defaultValue);
-    }
-  }, [props]);
-
-  const setInitialSelectedDay = (defaultValue: Date) => {
-
-    if (minDate && defaultValue.getTime() < minDate.getTime()) {
-      console.warn("'value' or 'defaultValue' cannot be before 'minDate'");
-      setSelectedDay(minDate);
-      return;
-    }
-
-    if (maxDate && defaultValue.getTime() > maxDate.getTime()) {
-      console.warn("'value' or 'defaultValue' cannot be after 'maxDate'");
-      if (minDate) {
-        setSelectedDay(minDate);
-        return;
-      }
-      setSelectedDay(new Date());
-      return;
-    }
-
-    setSelectedDay(defaultValue);
-    return;
-  };
-
-  const checkIsValidDay = (selectedDay: Date) => {
-
-    if (minDate && maxDate) {
-      return selectedDay &&
-        selectedDay.getTime() > minDate.getTime() &&
-        selectedDay.getTime() < maxDate.getTime();
-    }
-
-    if (minDate) {
-      return selectedDay &&
-        selectedDay.getTime() > minDate.getTime();
-    }
-
-    if (maxDate) {
-      return selectedDay &&
-        selectedDay.getTime() < maxDate.getTime();
-    }
-
-    return !!selectedDay;
-  };
 
   const { formatDate, parseDate } = MomentLocaleUtils;
 
-  const focusStartInput = () => {
-    dateInputRef.current?.focus()
-  }
-
-  const handleDayChange = async (selectedDay: Date) => {
-    const isValidDay = checkIsValidDay(selectedDay);
-
-    setIsValidDay(isValidDay);
-    await setSelectedDay(selectedDay);
-    focusStartInput();
-
+  const handleDayChange = (selectedDay: Date) => {
     onChange(selectedDay);
   }
 
-  const getInputPlaceholder = () => (
-    minDate ? minDate
-      : initialMonth ? initialMonth
-        : new Date()
-  );
-
-  const navButtonsClasses = cn(
-    "inline-block w-4 h-4",
-    "bg-center bg-no-repeat text-surface-400",
-    "cursor-pointer hover:opacity-80"
-  );
-
   const dayPickerClassNames = {
-    container: cn("bg-white inline-block font-montserrat text-base rounded shadow-md"), // The container element
-    wrapper: cn("bg-white relative rounded"), // The wrapper element: "", used for keyboard interaction
-    interactionDisabled: cn("cursor-default"), // Added to the container when there's no interaction with the calendar
-
-    navBar: cn("absolute flex items-center justify-between py-3 px-5 w-full border-b border-surface-100"), // The navigation bar with the arrows to switch between months
-    navButtonPrev: navButtonsClasses, // Button to switch to the previous month
-    navButtonNext: navButtonsClasses, // Button to switch to the next month
-    navButtonInteractionDisabled: cn("hidden"), // Added to the navbuttons when disabled with fromMonth/toMonth props
-
-    months: cn("flex flex-wrap justify-center px-3 pb-3"), // Container of the months table
-    month: cn("table my-0 font-bold text-center capitalize select-none border-collapse"), // The month's main table
-    caption: cn("table-caption py-2 px-2 text-center rounded-t"), // The caption element: "", containing the current month's name and year
-    weekdays: cn("table-header-group font-bold text-sm uppercase"), // Table header displaying the weekdays names
-    weekdaysRow: cn("table-row"), // Table row displaying the weekdays names
-    weekday: cn("table-cell w-8 p-2 text-surface-600 text-center text-sm"), // Cell displaying the weekday name
-    body: cn("table-row-group mx-5 mb-5 font-rubik font-normal text-xs leading-6.5"), // Table's body with the weeks
-    week: cn("table-row"), // Table's row for each week
-    day: cn("table-cell p-2 rounded align-middle text-center cursor-pointer hover:bg-surface-100"), // The single day cell
-
-    footer: cn("pt-2"), // The calendar footer (only with todayButton prop)
-    todayButton: cn("bg-transparent text-error-800 text-sm text-center border-0 cursor-pointer shadow-none"), // The today button (only with todayButton prop)
-
-    today: cn({ ["bg-primary-500 text-white hover:bg-primary-500 hover:text-white"]: !selectedDay || selectedDay === new Date() }), // Added to the day's cell for the current day
-    selected: cn("bg-primary-500 text-white hover:bg-primary-500"), // Added to the day's cell specified in the "selectedDays" prop
-    disabled: cn("text-surface-200 cursor-default"), // Added to the day's cell specified in the "disabledDays" prop
-    outside: cn("text-surface-200"), // Added to the day's cell outside the current month
-    weekNumber: ""
+    container: "bg-white inline-block font-montserrat text-base rounded shadow-md",
+    body:"table-row-group mx-5 mb-5 font-rubik font-normal text-xs leading-6.5",
+    caption: "table-caption py-2 px-2 text-center rounded-t",
+    day: "table-cell p-2 rounded align-middle text-center cursor-pointer hover:bg-surface-100",
+    disabled: "text-surface-200",
+    footer: "pt-2",
+    interactionDisabled: "cursor-not-allowed",
+    month: "table my-0 font-bold text-center capitalize select-none border-collapse",
+    months: "flex flex-wrap justify-center px-3 pb-3",
+    navBar: "absolute flex items-center justify-between py-3 px-5 w-full border-b border-surface-100",
+    navButtonInteractionDisabled: "hidden",
+    navButtonNext: "inline-block w-4 h-4 bg-center bg-no-repeat text-surface-400 cursor-pointer hover:opacity-80",
+    navButtonPrev: "inline-block w-4 h-4 bg-center bg-no-repeat text-surface-400 cursor-pointer hover:opacity-80",
+    outside: "text-surface-200",
+    selected: "bg-primary-500 text-white hover:bg-primary-500",
+    today: "bg-primary-500 text-white hover:bg-primary-500",
+    todayButton: "bg-transparent text-error-800 text-sm text-center border-0 cursor-pointer shadow-none",
+    week: "table-row",
+    weekNumber: "",
+    weekday: "table-cell w-8 p-2 text-surface-600 text-center text-sm",
+    weekdays: "table-header-group font-bold text-sm",
+    weekdaysRow: "table-row",
+    wrapper: "relative flex-row select-none bg-white rounded",
   };
 
   const dayPickerInputClasses = {
     container: "", // The day picker input container
-    overlay: cn("absolute z-10"), // The day picker overlay
-    overlayWrapper: cn("flex justify-center w-full")
+    overlay: "absolute z-10", // The day picker overlay
+    overlayWrapper: "flex justify-center w-full"
   };
 
   const _dayPickerProps: DayPickerProps = {
@@ -176,10 +93,10 @@ const DateInput: React.FC<IDateInput> = (props: IDateInput) => {
     fromMonth: minDate,
     toMonth: maxDate,
     classNames: dayPickerClassNames,
-    locale: locale,
+    locale: "es-mx",
     localeUtils: MomentLocaleUtils,
     modifiers: modifiers,
-    navbarElement: (navbarElementProps: NavbarElementProps) => <Navbar focusStartInput={focusStartInput} {...navbarElementProps} />,
+    navbarElement: (navbarElementProps: NavbarElementProps) => <Navbar {...navbarElementProps} />,
     showOutsideDays: showOutsideDays && canChangeMonth,
     weekdayElement: (weekDayElementProps: WeekdayElementProps) => <Weekday {...weekDayElementProps} />,
     ...dayPickerProps
@@ -188,60 +105,64 @@ const DateInput: React.FC<IDateInput> = (props: IDateInput) => {
   return (
     <DayPickerInput
       classNames={dayPickerInputClasses}
-      component={
-        React.forwardRef(
-          (props: any, ref: React.Ref<HTMLInputElement>) =>
-            <InputGroup
-              isValid={isValidDay}
-              rightElement={
-                <CalendarIcon className="w-6 h-6" />
-              }
-              ref={ref}
-              {...props}
-            />
-        )
-      }
+      component={DatePickerCustomInput}
       dayPickerProps={_dayPickerProps}
-      format={format}
+      format="L"
       formatDate={formatDate}
       parseDate={parseDate}
-      placeholder={
-        `${formatDate(selectedDay || getInputPlaceholder(), format, locale)}`
-      }
+      placeholder={placeholder}
       inputProps={{
         id: id,
-        ref: dateInputRef
+        isValid: valid,
+        disabled: disabled
       }}
       onDayChange={handleDayChange}
-      value={selectedDay}
+      value={defaultValue}
     />
   );
 };
 
-const Navbar = (props: any) => {
-  const { onPreviousClick, onNextClick, className, showPreviousButton, showNextButton, focusStartInput } = props;
+const DatePickerCustomInput = React.forwardRef(
+  (props, ref: React.Ref<HTMLInputElement>) =>
+    <InputGroup
+      rightElement={
+        <svg className="w-6 h-6" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M46 6a2 2 0 1 0-4 0v2H22V6a2 2 0 1 0-4 0v2h-6a4 4 0 0 0-4 4v40a4 4 0 0 0 4 4h40a4 4 0 0 0 4-4V12a4 4 0 0 0-4-4h-6V6Zm6 6h-6v2a2 2 0 1 1-4 0v-2H22v2a2 2 0 1 1-4 0v-2h-6v8h40v-8ZM12 24h40v28H12V24Zm9 8a2 2 0 0 1 2-2h7a2 2 0 0 1 1.562 3.248l-2.216 2.77a5.999 5.999 0 1 1-7.586 9.226 2 2 0 0 1 2.827-2.83A2 2 0 1 0 26 39a2 2 0 0 1-1.562-3.25l1.4-1.75H23a2 2 0 0 1-2-2Zm21 0a2 2 0 0 0-3.2-1.6l-4 3a2 2 0 1 0 2.4 3.2l.8-.6v9a2 2 0 0 0 4 0V32Z" fill="currentColor"/></svg>
+      }
+      ref={ref}
+      {...props}
+    />
+)
 
-  const btnHiddenClasses = cn("opacity-0 cursor-default");
+const Navbar = (props: NavbarElementProps) => {
+  const { onPreviousClick, onNextClick, className, showPreviousButton, showNextButton } = props;
+
+  const btnHiddenClasses = "opacity-0 cursor-default";
 
   return (
     <div className={className}>
       <button
         className={cn({ [btnHiddenClasses]: !showPreviousButton })}
-        onClick={() => onPreviousClick(focusStartInput)}
+        onClick={() => onPreviousClick()}
       >
-        <CaretLeftIcon className="w-4 h-4" />
+        <svg className="w-4 h-4" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M18 32a3 3 0 0 1 .929-2.17l22-21a3 3 0 1 1 4.142 4.34L25.345 32 45.07 50.83a3 3 0 0 1-4.142 4.34l-22-21A3 3 0 0 1 18 32Z" fill="currentColor"/></svg>
       </button>
       <button
         className={cn({ [btnHiddenClasses]: !showNextButton })}
-        onClick={() => onNextClick(focusStartInput)}
+        onClick={() => onNextClick()}
       >
-        <CaretRightIcon className="w-4 h-4" />
+        <svg className="w-4 h-4" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M46 32a3 3 0 0 1-.929 2.17l-22 21a3 3 0 0 1-4.142-4.34L38.655 32 18.93 13.17a3 3 0 0 1 4.142-4.34l22 21A3 3 0 0 1 46 32Z" fill="currentColor"/></svg>
       </button>
     </div>
   );
 }
 
-const Weekday = ({ weekday, localeUtils, className, locale }: WeekdayElementProps) => {
+const Weekday = (props: WeekdayElementProps) => {
+  const {
+    weekday,
+    localeUtils,
+    className,
+    locale
+  } = props;
   const weekdayName = localeUtils.formatWeekdayLong(weekday, locale);
 
   return (
