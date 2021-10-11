a900e52c2c1eb617d90457b406b00747b3e31ec8
JoseLAmador <josel.amador.s@gmail.com>
2021-10-11 13:31:51 -0500

    feat: add navbar component

--- packages/betomic/src/components/index.ts
+++ packages/betomic/src/components/index.ts
@@ -8,3 +8,4 @@ export { default as Alert } from "./Alert";
 export { default as Avatar } from "./Avatar";
 export { default as Container } from "./Container";
 export { default as Drawer } from "./Drawer";
+export { default as Navbar } from "./Navbar";
