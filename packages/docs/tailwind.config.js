52f77699c4a24192b9c733c5a26ab8aa0e49e33b
JoseLAmador <josel.amador.s@gmail.com>
2021-10-12 11:04:00 -0500

    styles: add custom classes to nav

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -139,6 +139,86 @@ module.exports = {
   },
   plugins: [
     require("./plugins/fonts"),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        ".navigation-header": {
+          "ul": {
+            "li": {
+              "a, button": {
+                position: "relative",
+                "&.active": {
+                  color: theme("colors.primary.500"),
+                  "&::after": {
+                    content: "''",
+                    position: "absolute",
+                    left: 0,
+                    bottom: 0,
+                    display: "block",
+                    height: theme("spacing.1"),
+                    borderRadius: theme("spacing.1"),
+                    width: "100%",
+                    backgroundColor: theme("colors.primary.500"),
+                    paddingLeft: theme("spacing.2"),
+                    paddingRight: theme("spacing.2")
+                  },
+                  "&:hover": {
+                    color: theme("colors.primary.500"),
+                    "&::after": {
+                      backgroundColor: theme("colors.primary.500")
+                    }
+                  },
+                },
+                "&:hover": {
+                  color: theme("colors.surface.600"),
+                  "&::after": {
+                    content: "''",
+                    position: "absolute",
+                    left: 0,
+                    bottom: 0,
+                    display: "block",
+                    height: theme("spacing.1"),
+                    borderRadius: theme("spacing.1"),
+                    width: "100%",
+                    paddingLeft: theme("spacing.2"),
+                    paddingRight: theme("spacing.2"),
+                    backgroundColor: theme("colors.surface.100")
+                  }
+                }
+              }
+            }
+          }
+        },
+
+        ".navigation-header.dark": {
+          backgroundColor: theme("colors.surface.800"),
+          "ul": {
+            "li": {
+              "a, button": {
+                color: theme("colors.white.DEFAULT"),
+                "&.active": {
+                  color: theme("colors.primary.500"),
+                  "&::after": {
+                    backgroundColor: theme("colors.primary.500"),
+                  },
+                  "&:hover": {
+                    color: theme("colors.primary.500"),
+                    "&::after": {
+                      backgroundColor: theme("colors.primary.500")
+                    }
+                  },
+                },
+                "&:hover": {
+                  color: theme("colors.white.DEFAULT"),
+                  "&::after": {
+                    backgroundColor: theme("colors.surface.500")
+                  }
+                }
+              }
+            }
+          }
+        },
+      })
+    }),
     plugin(function({ addUtilities }) {
       const filterUtilities = {
         ".grid p:not(:first-child), .grid p": {
@@ -160,6 +240,9 @@ module.exports = {
       addBase({
         table: {
           width: theme("width.full"),
+        },
+        ".nextra-container nav": {
+          boxShadow: "none !important"
         },
         "table tbody tr:nth-child(2n)": {
           backgroundColor: "inherit"
