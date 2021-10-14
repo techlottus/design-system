74f669629f890fd1f62c4e679225f28406af9397
JoseLAmador <josel.amador.s@gmail.com>
2021-10-14 16:51:20 -0500

    fix: export tailwind plugins

--- packages/betomic/src/tailwind/index.ts
+++ packages/betomic/src/tailwind/index.ts
@@ -1,2 +1,5 @@
 // @ts-ignore
 export { default as config } from "./base";
+export { default as drawer } from "./plugins/drawer";
+export { default as navigation } from "./plugins/navigation";
+export { default as tooltip } from "./plugins/tooltip";
