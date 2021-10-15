e320d234d21c30672d055a97b48148d38e280a00
Javier <coderdiaz@gmail.com>
2021-10-15 10:10:10 -0500

    style: changed to single quotes

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,61 +1,61 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
 const nextraTheme = {
   presets: [
-    require("@exponentialeducation/tailwind-base").default,
+    require('@exponentialeducation/tailwind-base').default,
   ],
   purge: {
     content: [
-      "./pages/**/*.{js,ts,tsx,mdx}",
-      "./components/**/*.{js,ts,tsx}",
-      "../../node_modules/@exponentialeducation/betomic/src/**/*.{js,ts,tsx}",
-      "../../node_modules/@exponentialeducation/datetime/src/**/*.{js,ts,tsx}",
-      "../../node_modules/@exponentialeducation/tooltip/src/**/*.{js,ts,tsx}",
-      "../../node_modules/@exponentialeducation/toast/src/**/*.{js,ts,tsx}"
+      './pages/**/*.{js,ts,tsx,mdx}',
+      './components/**/*.{js,ts,tsx}',
+      '../../node_modules/@exponentialeducation/betomic/src/**/*.{js,ts,tsx}',
+      '../../node_modules/@exponentialeducation/datetime/src/**/*.{js,ts,tsx}',
+      '../../node_modules/@exponentialeducation/tooltip/src/**/*.{js,ts,tsx}',
+      '../../node_modules/@exponentialeducation/toast/src/**/*.{js,ts,tsx}'
     ],
     options: {
       safelist: [
-        "nextra-container"
+        'nextra-container'
       ]
     }
   },
   theme: {},
-  darkMode: "class", // or "media" or "class"
+  darkMode: 'class', // or 'media' or 'class'
   variants: {
     extend: {},
   },
   plugins: [
-    require("./plugins/fonts"),
+    require('./plugins/fonts'),
     plugin(function({ addUtilities }) {
       const filterUtilities = {
-        ".grid p:not(:first-child), .grid p": {
+        '.grid p:not(:first-child), .grid p': {
           margin: 0,
         },
       }
 
-      addUtilities(filterUtilities, ["responsive", "hover"])
+      addUtilities(filterUtilities, ['responsive', 'hover'])
     }),
     plugin(function({ addBase, theme }) {
       addBase({
         table: {
-          width: theme("width.full"),
+          width: theme('width.full'),
         },
-        ".nextra-container": {
-          "nav": {
-            boxShadow: "none !important"
+        '.nextra-container': {
+          'nav': {
+            boxShadow: 'none !important'
           }
         },
-        "table tbody tr:nth-child(2n)": {
-          backgroundColor: "inherit"
+        'table tbody tr:nth-child(2n)': {
+          backgroundColor: 'inherit'
         },
-        ".dark table tbody tr:nth-child(2n)": {
-          backgroundColor: "#111111"
+        '.dark table tbody tr:nth-child(2n)': {
+          backgroundColor: '#111111'
         },
-        "table thead": {
-          backgroundColor: "#f7fafc"
+        'table thead': {
+          backgroundColor: '#f7fafc'
         },
-        ".dark table thead": {
-          backgroundColor: "#1a202c"
+        '.dark table thead': {
+          backgroundColor: '#1a202c'
         },
       })
     }),
