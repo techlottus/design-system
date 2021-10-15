71540f585af9d9576e2307e43d28646d37b665df
Javier <coderdiaz@gmail.com>
2021-10-15 01:44:31 -0500

    docs: added example for test lib

---
+++ packages/tailwind-base/example/pages/index.js
@@ -0,0 +1,14 @@
+const IndexPage = () => {
+  return <div>
+    <button className="px-4 py-2 bg-primary-500 text-white rounded-sm">Hello World</button>
+    <div className="navigation-header">
+      <ul>
+        <li>
+          <a className="active" href="#">Hello World</a>
+        </li>
+      </ul>
+    </div>
+  </div>;
+}
+
+export default IndexPage;
