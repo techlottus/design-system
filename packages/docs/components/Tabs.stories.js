9dc95d8a04150274573c74394585e7d31b0b5368
Maria-cris <maria.delacruz@lottuseducation.com>
2023-05-30 10:12:09 -0600

    stories

---
+++ packages/docs/components/Tabs.stories.js
@@ -0,0 +1,24 @@
+import Tabs from "./Tabs";
+export default {
+  title: "Tabs",
+  component: Tabs,
+  tags: ["autodocs"],
+  args: {
+    tabs: [
+      { label: "Text Link", active: true, iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+      { label: "Text Link", iconName: "home" },
+    ],
+  },
+};
+
+const Template = (args) => <Tabs {...args} />;
+
+const Standard = Template.bind({});
+
+export { Standard };
---
+++ packages/docs/components/TextLink.stories.js
@@ -0,0 +1,32 @@
+import TextLink from "./TextLink";
+export default {
+  title: "TextLink",
+  component: TextLink,
+  tags: ["autodocs"],
+  args: {
+    text: "Text Link",
+    href: "https://cat-bounce.com/",
+    target: "_self",
+    iconName: "home",
+    className: "",
+    onClick: () => {
+      console.log("function");
+    },
+    disabled: false,
+    items: false,
+    active: false,
+    iconPosition: "left",
+  },
+  argTypes: {
+    iconPosition: {
+      options: ["left", "right"],
+      controls: { type: "radio" },
+    },
+  },
+};
+
+const Template = (args) => <TextLink {...args} />;
+
+const Standard = Template.bind({});
+
+export { Standard };
