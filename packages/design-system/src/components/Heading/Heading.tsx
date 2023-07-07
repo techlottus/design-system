029546c8044ab3401ac86d0950eb8b031b687fcc
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-07 16:01:28 -0600

    chore: heading types implemented

--- packages/design-system/src/components/Heading/Heading.tsx
+++ packages/design-system/src/components/Heading/Heading.tsx
@@ -1,10 +1,11 @@
 import cn from "classnames";
+import { FontsType, HeadingType, VariantsType } from "../Types/Heading.types";
 
-const fonts: any = {
+const fonts: FontsType = {
   principal: cn("font-principal"),
   secondary: cn("font-secondary"),
 };
-const types: any = {
+const variants: VariantsType = {
   "h-1": cn("text-4xl md:text-6xl lg:text-7xl font-extrabold"),
   "h-2": cn("text-2xl md:text-4xl lg:text-6xl font-extrabold"),
   "h-3": cn("text-2xl lg:text-5xl md:text-3xl md:font-extrabold font-bold"),
@@ -12,30 +13,14 @@ const types: any = {
   "h-5": cn("text-base lg:text-xl font-bold"),
   "h-6": cn("text-base lg:text-lg font-semibold"),
 };
-const variants: any = {
-  primary: cn("text-neutral-500"),
-  negative: cn("text-neutral-100"),
-};
-const defaultValues = {
-  type: "h-1",
-  variant: "normal",
-  font: "principal",
-  className: "",
-};
 
-const Heading = (props: any) => {
-  const {
-    title,
-    type = defaultValues.type,
-    variant = defaultValues.variant,
-    font = defaultValues.font,
-    className = defaultValues.className,
-  } = props;
-  const headingClasses = cn(className, {
-    [fonts[font]]: true,
-    [types[type]]: true,
-    [variants[variant]]: true,
-  });
+const Heading: React.FC<HeadingType> = (props: HeadingType) => {
+  const { title, variant = "h-1", font = "principal", className = "" } = props;
+  const headingClasses: string = cn(
+    className,
+    [fonts[font]],
+    [variants[variant]]
+  );
   return <span className={headingClasses}>{title}</span>;
 };
 
