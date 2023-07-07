2786ecbbfa6528b314196ec7bbc50b511cee5047
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-07 16:01:13 -0600

    chore: heading types file created

---
+++ packages/design-system/src/components/Types/Heading.types.ts
@@ -0,0 +1,19 @@
+export interface FontsType {
+  principal: string;
+  secondary: string;
+}
+export interface VariantsType {
+  "h-1": string;
+  "h-2": string;
+  "h-3": string;
+  "h-4": string;
+  "h-5": string;
+  "h-6": string;
+}
+
+export interface HeadingType {
+  title: string;
+  variant?: "h-1" | "h-2" | "h-3" | "h-4" | "h-5" | "h-6";
+  font?: "principal" | "secondary";
+  className?: string;
+}
