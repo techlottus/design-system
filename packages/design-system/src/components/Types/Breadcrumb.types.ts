e3f685d0bbdd1771d15548b7c94aee0c03077953
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-11 13:21:17 -0600

    Revert "chore: add breadcrumb  types file"
    This reverts commit 6e971149c695ecee14db5eec9a912819ab71aa05.

--- packages/design-system/src/components/Types/Breadcrumb.types.ts
+++
@@ -1,8 +0,0 @@
-export interface BreadcrumbType {
-  homeIcon?: { iconName: string; route: string };
-  separatorIcon?: string;
-  collapseIcon?: string;
-  links: { text: string; route: string }[];
-  className?: string;
-  collapseMin?: number;
-}
