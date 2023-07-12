9504e4a51a02b29f9346b199e69dbc89043ea5c1
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-07-12 17:23:54 -0600

    feat: add types to SocialMedia component

--- packages/design-system/src/components/SocialMedia/SocialMedia.tsx
+++ packages/design-system/src/components/SocialMedia/SocialMedia.tsx
@@ -1,10 +1,11 @@
 import { myhref } from "../helpers/myrefHelper";
 import cn from "classnames";
 import SVGIcons from "../assets/SVGIcons";
+import { SocialMediaConfig, socialMediaItem } from "../Types/SocialMedia.types";
 
 const smClass = cn("cursor-pointer ");
 
-const SocialMedia = (props: any) => {
+const SocialMedia: React.FC<SocialMediaConfig> = (props: SocialMediaConfig) => {
   const { className = "", socialMedia, svgClass = "" } = props;
   return (
     <div
@@ -13,7 +14,7 @@ const SocialMedia = (props: any) => {
         "flex px-10 py-10 w-full justify-center space-x-2 "
       )}
     >
-      {socialMedia?.map((sm: any, index: any) => (
+      {socialMedia?.map((sm: socialMediaItem, index: number) => (
         <div
           className={cn(smClass)}
           id={sm?.alt}
