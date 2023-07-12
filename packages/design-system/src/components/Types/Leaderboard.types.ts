b2bdeb5f86cba59043b80705d76323ca453bba47
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-12 15:15:01 -0600

    chore: typing

---
+++ packages/design-system/src/components/Types/Leaderboard.types.ts
@@ -0,0 +1,24 @@
+import { IconType } from "./Icon.types";
+import { ButtonType } from "./button.types";
+
+export interface LeaderboardType {
+  imageUrl: string;
+  title?: string;
+  variant?: "dark" | "light";
+  links?: ExtensionsType[];
+  button?: ButtonType;
+}
+
+export interface LeaderboardTypeVariants {
+  light?: string;
+  dark?: string;
+}
+
+export interface ExtensionsType {
+  variant?: string;
+  ext?: string;
+  text?: string;
+  icon?: IconType;
+  target?: string;
+  className?: string;
+}
