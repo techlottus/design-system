a98212e77641c09ad9187409de58456898306daa
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-13 17:25:04 -0600

    chore: icon types modified

--- packages/design-system/src/components/Types/Icon.types.ts
+++ packages/design-system/src/components/Types/Icon.types.ts
@@ -1,9 +1,5 @@
 export type IconType = {
-<<<<<<< HEAD
-  iconName?: string;
-=======
   iconName?: string | undefined;
->>>>>>> feat/5965
   variant?: "solid" | "outlined";
   className?: string;
 };
