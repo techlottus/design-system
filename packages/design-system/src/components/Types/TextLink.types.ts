a516226757887e5e764c3532257667020ed4195f
Maria-cris <maria.delacruz@lottuseducation.com>
2023-07-13 18:11:54 -0600

    chore: textlink types modified

--- packages/design-system/src/components/Types/TextLink.types.ts
+++ packages/design-system/src/components/Types/TextLink.types.ts
@@ -1,17 +1,16 @@
-export interface TextLinkConfig  {
-    target: string;
-    className?: string;
-    disabled?: boolean;
-    active: boolean;
-    items: boolean;
-    iconPosition: 'right' | 'left';
-    href: string;
-    text: string;
-    iconName?: string;
-    onClick: () => void;
+export interface TextLinkConfig {
+  target?: string;
+  className?: string;
+  disabled?: boolean;
+  active?: boolean;
+  items?: boolean;
+  iconPosition?: "right" | "left";
+  href?: string;
+  text?: string;
+  iconName?: string;
+  onClick?: () => void;
 }
 export interface IconPosition {
-    right: string,
-    left: string
+  right?: string;
+  left?: string;
 }
-
