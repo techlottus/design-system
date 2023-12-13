1c0f18b1cbfbc9c8b83bdbecc46dc0f0c4d6ba20
Maria-cris <maria.delacruz@lottuseducation.com>
2023-12-13 16:57:43 -0600

    core: install tailwind base updated

--- storybook/tailwind.config.ts
+++ storybook/tailwind.config.ts
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import * as tailwindBaseLottus from "./tenants/LOTTUS/tailwindBase"
-import * as tailwindBase from "@lottuseducation/tailwind-base";
+import * as tailwindBase from "@lottuseducation/tailwind-base/lib/tailwind-base.js";
 
 module.exports = {
   content: [
     "./stories/**/*.stories.{js,jsx,ts,tsx}",
-    "./node_modules/@lottuseducation/design_system/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@lottuseducation/slider/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@lottuseducation/carousel/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@lottuseducation/numbers/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
     ,
   ],
   presets: [tailwindBaseLottus.default, tailwindBase.default],
