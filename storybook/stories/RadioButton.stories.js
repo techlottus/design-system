a82f94e3f73e508aca1c9af7b3f4e602138a5357
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-02-01 15:27:49 -0600

    define the stories of the radiogroup and the radiobutton components

---
+++ storybook/stories/RadioButton.stories.js
@@ -0,0 +1,42 @@
+import React from "react";
+import RadioButton from "../RadioButton/RadioButton"
+import RadioSimpleGroup from "../RadioButton/RadioGroup"
+import mdx from "../RadioButton/RadioButton.mdx"
+
+/** Radio Button give you the same functionality as native HTML radio inputs, without any of the styling. They're perfect for building out custom UIs for selectors.*/
+export default {
+  title: "MOLECULES/Radio Button",
+  component: RadioButton,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  args: {
+    className: "",
+    size: "md"
+  }
+
+};
+
+const Template = (args) => (
+  <RadioButton {...args}>
+  </RadioButton>
+);
+
+const TemplateTwo = (args) => (
+  <RadioSimpleGroup {...args}>
+  </RadioSimpleGroup>
+);
+
+const Standard = Template.bind({
+
+});
+
+const Group = TemplateTwo.bind({
+
+});
+
+
+export { Standard, Group };
