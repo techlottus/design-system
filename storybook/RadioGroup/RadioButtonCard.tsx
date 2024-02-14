934826b4850c46c1d50efb0c4236e3e3f645a044
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-02-14 15:31:56 -0600

    implement icons in radiobutton

--- storybook/RadioGroup/RadioButtonCard.tsx
+++ storybook/RadioGroup/RadioButtonCard.tsx
@@ -2,45 +2,124 @@ import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
 import React from "react";
 import Icon from "../components/Icon";
+import RadioButtonActive from '../../assets/icons/RadioButtonActive';
+import RadioButtonDefault from '../../assets/icons/RadioButtonDefault';
+
+const plans = [
+    {
+        name: 'Startup',
+        ram: '12GB',
+        cpus: '6 CPUs',
+        disk: '160 GB SSD disk',
+    },
+    {
+        name: 'Business',
+        ram: '16GB',
+        cpus: '8 CPUs',
+        disk: '512 GB SSD disk',
+    },
+    {
+        name: 'Enterprise',
+        ram: '32GB',
+        cpus: '12 CPUs',
+        disk: '1024 GB SSD disk',
+    },
+]
 
 const RadioButtonCard = () => {
-    const [selected, setSelected] = useState(true)
+
+    const [selected, setSelected] = useState(plans[0])
 
     return (
-        <div className="w-full px-1 grid grid-cols-12">
-            <div className="col-span-7 mx-auto w-96">
-                <RadioGroup value={selected} onClick={() => setSelected(!selected)}>
-                    <div className="space-y-2">
-                        <RadioGroup.Option
-                            value={true}
-                            className="relative flex cursor-pointer rounded-lg px-5 py-4 shadow-[0_2px_12px_0px_rgba(0,0,0,0.12)]">
-                            <div className="flex w-full items-center justify-between">
-                                <div className="flex items-center">
-                                    <Icon iconName="school" className="!font-icons-solid !text-4xl text-primary-500 !me-6" />
-                                    <div className="!grid !gap-3">
-                                        <RadioGroup.Label
-                                            as="p"
-                                            className="!text-gray-900 !text-lg !font-bold !mb-0">
-                                            Historial académico
-                                        </RadioGroup.Label>
-                                        <RadioGroup.Description
-                                            as="span"
-                                            className="text-gray-500 grid gap-3">
-                                            <p className="!my-0 !text-sm">Vencimiento: 08-06-2022</p>
-                                            <p className="!my-0 !text-base !font-bold !text-primary-500">$390.00</p>
-                                            <p className="!my-0 !text-base !text-primary-500">Eliminar</p>
-                                        </RadioGroup.Description>
-                                    </div>
-                                    {selected ?
-                                        <Icon iconName="radio_button_unchecked" className="!font-icons-solid !text-4xl !me-3 !text-surface-400 !absolute !right-0 !top-1" />
-                                        : <Icon iconName="radio_button_checked" className="!font-icons-solid !text-4xl !me-3 !text-surface-400 !absolute !right-0 !top-1" />
-                                    }
+        <div className="w-full">
+            <RadioGroup className="flex" value={selected} onChange={setSelected}>
+                <div className="space-y-2">
+                    <RadioGroup.Option
+                        key={plans[0].name}
+                        value={plans[0]}
+                        className="w-96 relative cursor-pointer rounded-lg px-5 py-4 shadow-[0_2px_12px_0px_rgba(0,0,0,0.12)]">
+                        <div className="flex w-full items-center justify-between">
+                            <div className="flex items-center">
+                                <Icon iconName="school" className="!font-icons-solid !text-4xl text-primary-500 !me-6" />
+                                <div className="!grid !gap-3">
+                                    <RadioGroup.Label
+                                        as="p"
+                                        className="!text-gray-900 !text-lg !font-bold !mb-0">
+                                        Historial académico
+                                    </RadioGroup.Label>
+                                    <RadioGroup.Description
+                                        as="span"
+                                        className="text-gray-500 grid gap-3">
+                                        <p className="!my-0 !text-sm">Vencimiento: 08-06-2022</p>
+                                        <p className="!my-0 !text-base !font-bold !text-primary-500">$390.00</p>
+                                        <p className="!my-0 !text-base !text-primary-500">Eliminar</p>
+                                    </RadioGroup.Description>
                                 </div>
+                                {selected == plans[0] ?
+                                    <RadioButtonActive size="24" fillColor="" className="!me-3 !absolute !right-0 !top-3" />
+                                    : <RadioButtonDefault size="24" fillColor="" className="!me-3 !absolute !right-0 !top-3" />
+                                }
                             </div>
-                        </RadioGroup.Option>
-                    </div>
-                </RadioGroup>
-            </div>
+                        </div>
+                    </RadioGroup.Option>
+                    <RadioGroup.Option
+                        key={plans[1].name}
+                        value={plans[1]}
+                        className="relative flex cursor-pointer !my-5 rounded-lg px-5 py-4 shadow-[0_2px_12px_0px_rgba(0,0,0,0.12)]">
+                        <div className="flex w-full items-center justify-between">
+                            <div className="flex items-center">
+                                <Icon iconName="credit_card" className="!font-icons-solid !text-4xl text-primary-500 !me-6" />
+                                <div className="!grid !gap-3">
+                                    <RadioGroup.Label
+                                        as="p"
+                                        className="!text-gray-900 !text-lg !font-bold !mb-0">
+                                        Historial crediticio
+                                    </RadioGroup.Label>
+                                    <RadioGroup.Description
+                                        as="span"
+                                        className="text-gray-500 grid gap-3">
+                                        <p className="!my-0 !text-sm">Vencimiento: 08-06-2022</p>
+                                        <p className="!my-0 !text-base !font-bold !text-primary-500">$250.00</p>
+                                        <p className="!my-0 !text-base !text-primary-500">Eliminar</p>
+                                    </RadioGroup.Description>
+                                </div>
+                                {selected == plans[1] ?
+                                    <RadioButtonActive size="24" fillColor="" className="!me-3 !text-surface-400 !absolute !right-0 !top-3" />
+                                    : <RadioButtonDefault size="24" fillColor="" className="!me-3 !text-surface-400 !absolute !right-0 !top-3" />
+                                }
+                            </div>
+                        </div>
+                    </RadioGroup.Option>
+                    <RadioGroup.Option
+                        key={plans[2].name}
+                        value={plans[2]}
+                        className="relative flex cursor-pointer rounded-lg px-5 py-4 shadow-[0_2px_12px_0px_rgba(0,0,0,0.12)]">
+                        <div className="flex w-full items-center justify-between">
+                            <div className="flex items-center">
+                                <Icon iconName="medical_services" className="!font-icons-solid !text-4xl text-primary-500 !me-6" />
+                                <div className="!grid !gap-3">
+                                    <RadioGroup.Label
+                                        as="p"
+                                        className="!text-gray-900 !text-lg !font-bold !mb-0">
+                                        Historial médico
+                                    </RadioGroup.Label>
+                                    <RadioGroup.Description
+                                        as="span"
+                                        className="text-gray-500 grid gap-3">
+                                        <p className="!my-0 !text-sm">Vencimiento: 08-06-2022</p>
+                                        <p className="!my-0 !text-base !font-bold !text-primary-500">$480.00</p>
+                                        <p className="!my-0 !text-base !text-primary-500">Eliminar</p>
+                                    </RadioGroup.Description>
+                                </div>
+                                {selected == plans[2] ?
+                                    <RadioButtonActive size="24" fillColor="" className="!me-3 !text-surface-400 !absolute !right-0 !top-3" />
+                                    : <RadioButtonDefault size="24" fillColor="" className="!me-3 !text-surface-400 !absolute !right-0 !top-3" />
+                                }
+                            </div>
+                        </div>
+                    </RadioGroup.Option>
+                </div>
+            </RadioGroup>
         </div>
     )
 };
--- storybook/RadioGroup/SelectChip.tsx
+++ storybook/RadioGroup/SelectChip.tsx
@@ -2,51 +2,53 @@ import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
 import React from "react";
 import Icon from '../components/Icon/Icon';
+import RadioButtonActive from '../../assets/icons/RadioButtonActive';
+import RadioButtonDefault from '../../assets/icons/RadioButtonDefault';
 
 const SelectChip = () => {
- let [plan, setPlan] = useState('startup')
+    let [selected, setSelected] = useState('startup')
 
- return (
-  <div className="w-full px-1 grid grid-cols-12">
-   <div className="col-span-5">
-    <RadioGroup value={plan} onChange={setPlan}>
-     <RadioGroup.Option value="startup" className="bg-surface-100 w-24 h-9	border-2 border-black rounded py-1.5">
-      {({ checked }) => (
-       <>
-        {checked ?
-         <Icon iconName="adjust" className="!font-icons-solid text-base !ms-2 !me-3 text-dark" />
-         : <Icon iconName="radio_button_unchecked" className="!font-icons-solid text-base !ms-2 !me-3 text-surface-400" />
-        }
-        <span className={checked ? 'bg-surface-100' : ''}>Label</span>
-       </>
-      )}
-     </RadioGroup.Option>
-     <RadioGroup.Option value="business" className="bg-surface-100 w-24 h-9 border-2 border-info-700 rounded py-1.5 !my-3">
-      {({ checked }) => (
-       <>
-        {checked ?
-         <Icon iconName="adjust" className="!font-icons-solid text-base !ms-2 !me-3 text-info-700" />
-         : <Icon iconName="radio_button_unchecked" className="!font-icons-solid text-base !ms-2 !me-3 text-surface-400" />
-        }
-        <span className={checked ? 'bg-surface-100' : ''}>Label</span>
-       </>
-      )}
-     </RadioGroup.Option>
-     <RadioGroup.Option value="enterprise" className="bg-surface-100 w-24 h-9 border-2 border-error-600 rounded py-1.5">
-      {({ checked }) => (
-       <>
-        {checked ?
-         <Icon iconName="adjust" className="!font-icons-solid text-base !ms-2 !me-3 text-error-600" />
-         : <Icon iconName="radio_button_unchecked" className="!font-icons-solid text-base !ms-2 !me-3 text-surface-400" />
-        }
-        <span className={checked ? 'bg-surface-100' : ''}>Label</span>
-       </>
-      )}
-     </RadioGroup.Option>
-    </RadioGroup>
-   </div>
-  </div>
- )
+    return (
+        <div className="w-full px-1 grid grid-cols-12">
+            <div className="col-span-5">
+                <RadioGroup value={selected} onChange={setSelected}>
+                    <RadioGroup.Option value="startup" className="flex bg-surface-100 w-24 h-9 cursor-pointer border-2 border-black rounded py-1.5">
+                        {({ checked }) => (
+                            <>
+                                {checked ?
+                                    <RadioButtonActive size="16" fillColor="#18181B" className="!ms-2 !me-3 my-auto" />
+                                    : <RadioButtonDefault size="16" fillColor="#18181B" className="!ms-2 !me-3 my-auto" />
+                                }
+                                <span className={checked ? 'bg-surface-100' : ''}>Label</span>
+                            </>
+                        )}
+                    </RadioGroup.Option>
+                    <RadioGroup.Option value="business" className="flex bg-surface-100 w-24 h-9 cursor-pointer border-2 border-info-700 rounded py-1.5 !my-3">
+                        {({ checked }) => (
+                            <>
+                                {checked ?
+                                    <RadioButtonActive size="16" fillColor="#2B897E" className="!ms-2 !me-3 my-auto" />
+                                    : <RadioButtonDefault size="16" fillColor="#2B897E" className="!ms-2 !me-3 my-auto" />
+                                }
+                                <span className={checked ? 'bg-surface-100' : ''}>Label</span>
+                            </>
+                        )}
+                    </RadioGroup.Option>
+                    <RadioGroup.Option value="enterprise" className="flex bg-surface-100 w-24 h-9 cursor-pointer border-2 border-error-600 rounded py-1.5">
+                        {({ checked }) => (
+                            <>
+                                {checked ?
+                                    <RadioButtonActive size="16" fillColor="#0033FF" className="!ms-2 !me-3  my-auto" />
+                                    : <RadioButtonDefault size="16" fillColor="#0033FF" className="!ms-2 !me-3 my-auto" />
+                                }
+                                <span className={checked ? 'bg-surface-100' : ''}>Label</span>
+                            </>
+                        )}
+                    </RadioGroup.Option>
+                </RadioGroup>
+            </div>
+        </div>
+    )
 };
 
 export default SelectChip;
