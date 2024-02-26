713a5cc24967ff06cb01fc87900263a33027b2f3
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-02-26 17:37:19 -0600

    index of each component

--- packages/icons/src/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CheckBoxActive } from './CheckBoxActive'
-export { default as CheckBoxDefault } from './CheckBoxDefault'
-export { default as RadioButtonActive } from './RadioButtonActive'
-export { default as RadioButtonDefault } from './RadioButtonDefault'
--- packages/icons/src/types.ts
+++
@@ -1,5 +0,0 @@
-export interface svgIcon {
-    fillColor: string,
-    size: "sm" | "md" | "lg",
-    className: string,
-}
