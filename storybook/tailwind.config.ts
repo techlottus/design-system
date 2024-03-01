c5582fb87741fa89c4b9c54f0f58223fb89a0e4b
Maria-cris <maria.delacruz@lottuseducation.com>
2024-02-29 18:32:46 -0600

    fix some clases

--- storybook/tailwind.config.ts
+++ storybook/tailwind.config.ts
@@ -3,9 +3,10 @@ import * as tailwindBaseLottus from "./tenants/LOTTUS/tailwindBase"
 import * as tailwindBase from "@lottuseducation/tailwind-base/lib/tailwind-base.js";
 
 module.exports = {
+  important:true,
   content: [
     "./stories/**/*.stories.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx,mdx}"
     ,
   ],
   presets: [tailwindBaseLottus.default, tailwindBase.default],
