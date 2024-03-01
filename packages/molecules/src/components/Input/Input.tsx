1a2b26a9bee2a51d973d781cf415186df12cca4a
Maria-cris <maria.delacruz@lottuseducation.com>
2024-02-29 18:33:24 -0600

    added invalid and valid cases

--- packages/molecules/src/components/Input/Input.tsx
+++ packages/molecules/src/components/Input/Input.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import cn from "classnames"
-import { IInput } from "../Types/Input.types";
-
-// Types
-type InputGroupType = {
-  RightElement?: React.FC<ElementType>;
-  LeftElement?: React.FC<ElementType>;
-  Input?: React.FC<IInput>;
-};
-type ElementType = { children?: React.ReactNode };
-
+import { ElementType, IInput, InputContainerType, InputGroupType } from "../Types/Input.types";
 
 //components
-const InputGroup: InputGroupType = ({ children, className }: { children?: React.ReactNode, className: string }) => <div className={cn(" flex space-x-2 border rounded", className)}>{children}</div>;
-const RightElement: React.FC<ElementType> = ({ children }: { children?: React.ReactNode }) => (
-  <div className="py-3 px-2 flex align-center order-last ">{children}</div>
+const InputGroup: InputGroupType = (props: InputContainerType) => {
+  const { children, className, isValid=null,disabled=false} = props;
+  return (
+    <div tabIndex={0} className={cn("flex border rounded h-10 input-group grow align-middle",
+    {["border-surface-200 focus-within:border-surface-600"]:isValid==null,
+    ["border-success-500 focus-within:border-success-500"]:isValid,
+    ["border-error-500 focus-within:border-error-500"]:!isValid,},{["border-surface-200 focus-within:border-surface-200"]:disabled}, className)} >
+      {children}
+    </div>);
+}
+
+const RightElement: React.FC<ElementType> = ({ children,className }: ElementType) => (
+  <div className={cn("py-3 pr-3 pl-2 flex align-center justify-center order-last ",className)}>{children}</div>
 );
-const LeftElement: React.FC<ElementType> = ({ children }: { children?: React.ReactNode }) => (
-  <div className="py-3 px-2 flex align-center order-first">{children}</div>
+const LeftElement: React.FC<ElementType> = ({ children,className }: ElementType) => (
+  <div className={cn("py-3 pl-3 pr-2 flex align-center justify-center order-first",className)}>{children}</div>
 );
 
 const Input = React.forwardRef((props: IInput, ref: React.Ref<HTMLInputElement>) => {
@@ -31,15 +32,13 @@ const Input = React.forwardRef((props: IInput, ref: React.Ref<HTMLInputElement>)
     ...restProps
   } = props;
 
-  const inputClasses = cn("flex grow peer", "w-full", "border-transparent focus:outline-none", "text-base placeholder-transparent ", className);
-  const containerClasses = cn("input-group relative grow flex align-middle peer-focus:my-3 mt-3 mb-1")
-  const labelClasses = cn("text-surface-800 text-xs font-texts  absolute scale-75 origin-[0] duration-300 transform -translate-y-2",
-    "peer-placeholder-shown:text-surface-300 peer-placeholder-shown:text-base  peer-placeholder-shown:translate-y-1",
-    "peer-focus:text-surface-800 peer-focus:text-xs peer-focus:-translate-y-2 ");
+  const inputClasses = cn("flex order-2 grow peer", "w-full", "border-transparent focus:outline-none pt-3 px-1", "text-base placeholder-transparent ", className);
+  const labelClasses = cn("order-2 text-xs font-texts  absolute scale-75 origin-center duration-300 transform -translate-y-8 ",
+    "peer-placeholder-shown:text-base peer-placeholder-shown:text-surface-700 peer-placeholder-shown:-translate-y-5 ",
+    "peer-focus:text-xs peer-focus:-translate-y-8 ",{["text-surface-800  peer-focus:text-surface-800"]:isValid==null,["text-success-500  peer-focus:text-success-500"]:isValid,["text-error-500  peer-focus:text-error-500"]:!isValid},{["text-surface-200  peer-placeholder-shown:text-surface-300"]:disabled});
 
   return (
-    <div className={containerClasses} >
-
+<div tabIndex={0} className="w-full input-group">
       <input
         id="input"
         placeholder={label}
@@ -49,11 +48,12 @@ const Input = React.forwardRef((props: IInput, ref: React.Ref<HTMLInputElement>)
         type={type}
         {...restProps} />
 
-      <label htmlFor="input" className={labelClasses}>{label}</label>
-    </div>
+      <label htmlFor="input" className={labelClasses}>{label}</label></div>
   )
 });
-//exports
+
+
+
 InputGroup.RightElement = RightElement;
 InputGroup.LeftElement = LeftElement;
 InputGroup.Input = Input;
--- packages/molecules/src/components/Types/Input.types.ts
+++ packages/molecules/src/components/Types/Input.types.ts
@@ -5,3 +5,11 @@ export interface IInput extends InputHTMLAttributes<HTMLInputElement> {
   isValid?: boolean | null;
   label: string;
 }
+
+export type InputGroupType = {
+  RightElement?: React.FC<ElementType>;
+  LeftElement?: React.FC<ElementType>;
+  Input?: React.FC<IInput>;
+};
+export type InputContainerType={children?: React.ReactNode, className: string, isValid?:boolean | null, disabled?:boolean}
+export type ElementType = { children?: React.ReactNode, className: string }
