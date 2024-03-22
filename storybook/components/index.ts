ddba66f9c5f47da4ec7ff8a53eab5b5ea3a1298e
Maria-cris <maria.delacruz@lottuseducation.com>
2024-03-22 16:52:47 -0600

    docs:  copy storybook

--- storybook/components/index.ts
+++ storybook/components/index.ts
@@ -1,1 +1,4 @@
-export { default as Button } from "./Button";
+export { CheckBoxActive } from "./CheckBoxActive";
+export { CheckBoxDefault } from "./CheckBoxDefault";
+export { RadioButtonActive } from "./RadioButtonActive"
+export { RadioButtonDefault } from "./RadioButtonDefault";
