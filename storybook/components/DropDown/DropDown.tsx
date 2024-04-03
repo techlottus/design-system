7d0f6ae12a20ce763b0c4d9d5ee0d06f625a8e15
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-03 16:48:14 -0600

    added width in items

--- storybook/components/DropDown/DropDown.tsx
+++ storybook/components/DropDown/DropDown.tsx
@@ -22,7 +22,7 @@ const  Button = (props:ElementType)=> {
 const  Items = (props:ElementType)=> {
   const {children}=props;
   return (
-      <Menu.Items  className="p-1 rounded h-50  scroll absolute mt-2 left-0 shadow">
+      <Menu.Items  className="p-1 rounded h-50  scroll absolute mt-2 left-0 shadow w-52">
          {children}     
       </Menu.Items>
   )
@@ -30,7 +30,7 @@ const  Items = (props:ElementType)=> {
 const  Item = (props:ElementType)=> {
   const {children}=props;
   return (
-      <Menu.Item as="div" className="flex p-3 space-x-2.5 hover:bg-surface-200 font-texts text-lg">
+      <Menu.Item as="div" className="flex p-3 space-x-2.5 hover:bg-surface-200 font-texts text-lg w-full">
          {children}     
       </Menu.Item>
   )
