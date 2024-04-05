ef2ca4951eaac91bcdabbe791bc0cdb56e254d2d
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-05 15:54:23 -0600

    docs: added examples

--- storybook/stories/DropDown.stories.js
+++ storybook/stories/DropDown.stories.js
@@ -19,18 +19,50 @@ export default {
 const Template = () =>{ 
   return(
     <DropDown className="shadow">
-    <DropDown.Button>Button</DropDown.Button>
+    <DropDown.Button><div className='flex justify-between p-3'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div></DropDown.Button>
     <DropDown.Items className="h-40">
-      <DropDown.Item >option</DropDown.Item>
-      <DropDown.Item >option</DropDown.Item>
-      <DropDown.Item >option</DropDown.Item>
-      <DropDown.Item >option</DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
     </DropDown.Items>    
     </DropDown>
 
 )}; 
+const Template2 = () =>{ 
+  return(
+    <DropDown className="shadow">
+    <DropDown.Button>
+    <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></DropDown.Button>
+    <DropDown.Items className="h-40">
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
+    </DropDown.Items>    
+    </DropDown>
+
+)}; 
+const Template3 = () =>{ 
+  return(
+    <div className="w-full shadow flex">
+    <DropDown className="order-last">
+    <DropDown.Button>
+    <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></DropDown.Button>
+    <DropDown.Items className="h-40">
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
+      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
+    </DropDown.Items>    
+    </DropDown>
+    </div>
+
+)}; 
  
 
-const InputStandar = Template.bind();
+const DropDownText = Template.bind();
+const DropDownImage = Template2.bind();
+const DropDownMenu = Template2.bind();
 
-export { InputStandar};
+export { DropDownText,DropDownImage, DropDownMenu};
