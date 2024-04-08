bf13e49e5707f1d3f9c7cd0336d7ebd43c1e9a00
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-08 14:52:43 -0600

    docs: dropdown nested

--- storybook/stories/DropDown.stories.js
+++ storybook/stories/DropDown.stories.js
@@ -70,7 +70,7 @@ const Template4 = () =>{
         <div className="z-20">
         <DropDown>
           <DropDown.Button>Option Nested</DropDown.Button>
-          <DropDown.Items className="left-52 z-20">
+          <DropDown.Items className="z-20">
             <DropDown.Item>option 1</DropDown.Item>
             <DropDown.Item>option 2</DropDown.Item>
           </DropDown.Items>
