9f46470f28ef2e6276b6712378a17984ef21d9c4
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-08 12:52:50 -0600

    docs: add styles

--- storybook/stories/DropDown.stories.js
+++ storybook/stories/DropDown.stories.js
@@ -67,13 +67,14 @@ const Template4 = () =>{
     <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></DropDown.Button>
     <DropDown.Items className="h-40">
       <DropDown.Item >
+        <div className="z-20">
         <DropDown>
           <DropDown.Button>Option Nested</DropDown.Button>
-          <DropDown.Items className="left-52 z-20" static>
+          <DropDown.Items className="left-52 z-20">
             <DropDown.Item>option 1</DropDown.Item>
             <DropDown.Item>option 2</DropDown.Item>
           </DropDown.Items>
-        </DropDown>
+        </DropDown></div>
       </DropDown.Item>
       <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
       <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
