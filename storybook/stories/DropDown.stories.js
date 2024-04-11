0525a5eb7b54f2ace4a2da67aa4001cb46834fa0
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-10 22:06:55 -0600

    docs styles

--- storybook/stories/DropDown.stories.js
+++ storybook/stories/DropDown.stories.js
@@ -20,25 +20,25 @@ const Template = () =>{
   return(
     <DropDown className="shadow relative">
     <DropDown.Button><div className='flex justify-between p-3 w-52'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div></DropDown.Button>
-    <DropDown.Items className="h-40 absolute">
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
+    <DropDown.Items className="h-40 absolute ">
+      <DropDown.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
+      <DropDown.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
+      <DropDown.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
+      <DropDown.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
     </DropDown.Items>    
     </DropDown>
 
 )}; 
 const Template2 = () =>{ 
   return(
-    <DropDown className="shadow">
+    <DropDown className="shadow relative">
     <DropDown.Button>
     <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></DropDown.Button>
-    <DropDown.Items className="h-40">
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
-      <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
+    <DropDown.Items className="h-40 absolute ">
+      <DropDown.Item  ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
+      <DropDown.Item  ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
+      <DropDown.Item  ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
+      <DropDown.Item  ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></DropDown.Item>
     </DropDown.Items>    
     </DropDown>
 
@@ -46,10 +46,10 @@ const Template2 = () =>{
 const Template3 = () =>{ 
   return(
     <div className="w-full shadow flex justify-end">
-    <DropDown >
+    <DropDown className="relative">
     <DropDown.Button>
     <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></DropDown.Button>
-    <DropDown.Items className="h-40">
+    <DropDown.Items className="h-40 absolute">
       <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></DropDown.Item>
       <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></DropDown.Item>
       <DropDown.Item ><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></DropDown.Item>
