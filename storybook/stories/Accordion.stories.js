cd4eb99051a3f631c69b5cd812151843670bd2ed
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-11 16:13:39 -0600

    docs: added color example

--- storybook/stories/Accordion.stories.js
+++ storybook/stories/Accordion.stories.js
@@ -69,22 +69,20 @@ const Template2 = () =>{
 
 </div>
 )}; 
-// const Template3 = () =>{ 
-//   return(
-//     <div className="w-full shadow flex justify-end">
-//     <Accordion className="relative">
-//     <Accordion.Button>
-//     <div className='flex space-x-2.5'><img src='https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg' width={'48px'} height={'48px'} /><div className='flex-col space-y-2 justify-start w-full'><div className='flex justify-between'><span className='font-bold font-texts'>Options</span><span className='font-icons-solid text-lg'>expand_more</span></div><div className='flex justify-items-start'><span className='rounded text-success-500 bg-success-100 p-1 text-sm'>Active</span></div></div></div></Accordion.Button>
-//     <Accordion.Items className="h-40 absolute">
-//       <Accordion.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 1</span></Accordion.Item>
-//       <Accordion.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 2</span></Accordion.Item>
-//       <Accordion.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 3</span></Accordion.Item>
-//       <Accordion.Item className="flex space-x-2.5"><span className='font-icons-solid text-lg'>people</span><span className='font-texts'>Option 4</span></Accordion.Item>
-//     </Accordion.Items>    
-//     </Accordion>
-//     </div>
+const Template3 = () =>{ 
+  return(
+    <Accordion>
+    <Accordion.Button className="flex justify-between border-success-500" iconOpen={<span className="font-icons-solid text-lg order-12 rounded-full bg-success-500 text-surface-50">expand_less </span>} iconClosed={<span className="font-icons-solid text-lg order-12 rounded-full bg-success-500 text-surface-50">expand_more </span>} openClasses=" bg-surface-50" closeClasses="bg-surface-10">
+      <div className="flex space-x-2.5  order-1 w-full"><span className="font-icons-outlined text-lg text-surface-300">people</span> <span className="font-bold font-texts">Title</span></div>
+      <div className="flex space-x-2.5 order-last">            
+         <span className="font-texts">Text</span></div>
+    </Accordion.Button>
+    <Accordion.Panel>
+    Panel
+  </Accordion.Panel> 
+</Accordion>
 
-// )}; 
+)}; 
  
 // const Template4 = () =>{ 
 
@@ -112,7 +110,7 @@ const Template2 = () =>{
 
 const AccordionExample = Template.bind();
  const AccordionList = Template2.bind();
-// const AccordionMenu = Template3.bind();
+const AccordionColor = Template3.bind();
 // const AccordionNested = Template4.bind();
 
-export { AccordionExample, AccordionList};
+export { AccordionExample, AccordionList, AccordionColor};
