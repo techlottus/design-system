b3c08602a2306e0d5f431fe8765c877123832092
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-17 14:32:20 -0600

    core: remove styles

--- packages/organisms/src/components/Accordion/Accordion.tsx
+++ packages/organisms/src/components/Accordion/Accordion.tsx
@@ -27,8 +27,7 @@ const Button = (props: ButtonType) => {
       {isOpen ? iconOpen : iconClosed}
     </Disclosure.Button>) 
     :
-     (<Disclosure.Button className={cn("p-4 flex space-x-2.5 w-full",{ ["rounded-lg border border-surface-200"]: !open,
-     ["rounded-t-lg border-t border-x bg-surface-200 border-surface-200"]: open }, className)} {...restprops}>
+     (<Disclosure.Button className={cn("p-4 flex space-x-2.5 w-full", className)} {...restprops}>
       {children}
       {open ? iconOpen : iconClosed}
     </Disclosure.Button>)
