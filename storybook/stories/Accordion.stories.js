8e63a039700efb2063ea636ad959e244cc919e37
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-19 15:15:20 -0600

    docs

--- storybook/stories/Accordion.stories.js
+++ storybook/stories/Accordion.stories.js
@@ -14,6 +14,7 @@ export default {
   },
   args: {
     variant: "outlined",
+    className:"flex justify-between border-surface-200"
   },
   argTypes:{
     variant: {
@@ -28,8 +29,7 @@ export default {
 const Template = (args) => {
   return (
     <Accordion>
-     <Accordion.Button className="flex justify-between border-surface-200"
-                       iconOpen={<span className="font-icons-solid text-lg order-12">expand_less </span>} 
+     <Accordion.Button iconOpen={<span className="font-icons-solid text-lg order-12">expand_less </span>} 
                        iconClosed={<span className="font-icons-solid text-lg order-12">expand_more </span>} {...args}>
        <div className="flex space-x-2.5  order-1 w-full"><span className="font-icons-outlined text-lg text-surface-300">person</span> <span className="font-bold font-texts">Title</span></div>
        <div className="flex space-x-2.5 order-last">
@@ -45,7 +45,7 @@ const Template = (args) => {
 const Template2 = (args) => {
   return (
     <Accordion>
-        <Accordion.Button  className={cn("flex justify-between  border-primary-500 ")}  iconOpen={<span className="font-icons-solid text-lg order-12">expand_less </span>} iconClosed={<span className="font-icons-solid text-lg order-12">expand_more </span>} {...args}>
+        <Accordion.Button    iconOpen={<span className="font-icons-solid text-lg order-2">expand_less </span>} iconClosed={<span className="font-icons-solid text-lg order-2">expand_more </span>} {...args}>
        <div className="flex space-x-2.5  order-1 w-full"><span className="font-icons-outlined text-lg text-surface-300">person</span> <span className="font-bold font-texts">Title</span></div>
        <div className="flex space-x-2.5 order-last">
          <span className="font-texts">Text</span></div>
