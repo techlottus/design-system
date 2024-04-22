2ee0c06183cf9dc243459b9928c852c0bbace0b6
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-22 16:48:34 -0600

    core: space

--- packages/molecules/src/components/TextArea/TextArea.tsx
+++ packages/molecules/src/components/TextArea/TextArea.tsx
@@ -12,7 +12,7 @@ const TextArea: TextAreaType = (props: TextAreaType) => {
 
   return (
     <div>
-      <div tabIndex={0} className="flex p-2 space-x-2.5 border focus-within:border-surface-600 rounded">
+      <div tabIndex={0} className="flex p-2 space-x-2 border focus-within:border-surface-600 rounded">
         <textarea className={cn("resize-none block w-full text-base overflow-y-auto outline-none",{
          ["bg-surface-100"]: variant === "flat",
         })} placeholder={placeholder} {...restProps}>
