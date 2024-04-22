5f6b9c88ada4eacf3e92a6ff5e154da6f9130486
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-22 11:13:53 -0600

    core: added textarea component in molecules package

--- packages/molecules/src/components/Types/TextArea.types.ts
+++ packages/molecules/src/components/Types/TextArea.types.ts
@@ -1,7 +1,11 @@
-export interface TextAreaType {
-    textField?: string;
-    variant: "outline" | "flat"
-    placeholder?: string;
-    children: React.ReactNode;
-  }
-  
+export type  TextAreaType ={
+    variant?: "outline" | "flat";
+    placeholder?:string;
+    children?: React.ReactNode;
+    className?:string;
+    RightElement?: React.FC<ElementType>;
+    LeftElement?: React.FC<ElementType>;
+  };
+
+
+export type ElementType = { children?: React.ReactNode, className?: string }
