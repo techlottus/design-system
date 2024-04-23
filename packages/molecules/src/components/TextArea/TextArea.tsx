e1053622fc2c7e207d644b0ba4728fc1aed6c451
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-23 12:03:27 -0600

    core disabled placeholder styles

--- packages/molecules/src/components/TextArea/TextArea.tsx
+++ packages/molecules/src/components/TextArea/TextArea.tsx
@@ -16,7 +16,7 @@ const TextArea: TextAreaType = (props: TextAreaType) => {
     <div>
       <div tabIndex={0} className={cn("flex py-2 pr-2 space-x-2",{["border rounded"]:variant==="outline",["border-b-error-500 rounded-b-none"]:error,["border-b rounded-t bg-surface-100"]:variant==="flat",["px-2"]:!children,["focus-within:border-surface-600 "]:!disabled})}>
         <textarea className={cn("resize-none block w-full text-base overflow-y-auto outline-none placeholder:text-surface-700",{
-         ["bg-surface-100"]: variant === "flat",["pl-2"]:children, ["text-surface-300 pointer-events-none"]:disabled,
+         ["bg-surface-100"]: variant === "flat",["pl-2"]:children, ["text-surface-300 pointer-events-none placeholder:text-surface-300"]:disabled,
         })} placeholder={placeholder} {...restProps}>
         </textarea>
         {children}
