9968efe6ed0cde89b84ba796b3add4be9351c0a5
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-23 12:10:11 -0600

    core: disabled styles

--- storybook/components/TextArea/TextArea.tsx
+++ storybook/components/TextArea/TextArea.tsx
@@ -15,7 +15,7 @@ const TextArea: TextAreaType = (props: TextAreaType) => {
   return (
     <div>
       <div tabIndex={0} className={cn("flex py-2 pr-2 space-x-2",{["border rounded"]:variant==="outline",["border-b-error-500 focus-within:border-b-error-500 rounded-b-none"]:error,["border-b rounded-t bg-surface-100"]:variant==="flat",["px-2"]:!children,["focus-within:border-x-surface-600 focus-within:border-t-surface-600 "]:!disabled && error,["focus-within:border-surface-600 "]:!disabled && !error})}>
-        <textarea className={cn("resize-none block w-full text-base overflow-y-auto outline-none placeholder:text-surface-700",{
+        <textarea className={cn("resize-none block w-full text-base overflow-y-auto outline-none ",{["placeholder:text-surface-700"]:!disabled,
          ["bg-surface-100"]: variant === "flat",["pl-2"]:children, ["text-surface-300 pointer-events-none placeholder:text-surface-300"]:disabled,
         })} placeholder={placeholder} {...restProps}>
         </textarea>
