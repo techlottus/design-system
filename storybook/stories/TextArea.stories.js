d2631b30bf099161e73b7797cda353b5efff11e9
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-23 11:35:39 -0600

    docs

--- storybook/stories/TextArea.stories.js
+++ storybook/stories/TextArea.stories.js
@@ -13,7 +13,8 @@ export default {
   args: {
     placeholder: "Placeholder",
     variant: "outline",
-    disabled:false
+    disabled:false,
+    error:false
   },
   argTypes:{
     variant: {
@@ -38,6 +39,15 @@ const ErrorExample = (args) => (
   <TextArea.LeftElement><span className="font-icons-solid text-surface-300 text-lg h-6 w-6 text-center">people</span></TextArea.LeftElement>
   </TextArea>
   <div className="flex justify-between"><span className="font-text text-error-500">Error text help</span> <span className="font-text text-surface-400">0/100</span></div>
+</div>
+);
+const TextFieldExample = (args) => (
+  <div className="flex-col">
+  <div className="flex justify-between"><span className="font-text text-surface-700">Text field description</span> <span className="font-icons-solid w-6 h-6 rounded-full bg-surface-500 text-surface-50">question_mark</span></div>
+  <TextArea error  {...args}><TextArea.RightElement><span className="font-icons-solid text-surface-300 text-lg h-6 w-6 text-center">favorite</span></TextArea.RightElement>
+  <TextArea.LeftElement><span className="font-icons-solid text-surface-300 text-lg h-6 w-6 text-center">people</span></TextArea.LeftElement>
+  </TextArea>
+  <div className="flex justify-between"><span className="font-text text-error-500">Text help</span> <span className="font-text text-surface-400">0/100</span></div>
 </div>
 );
 
