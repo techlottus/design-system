3754034ce082a1d2bb37268b457dd63aa3873d61
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-23 11:50:05 -0600

    docs

--- storybook/stories/TextArea.stories.js
+++ storybook/stories/TextArea.stories.js
@@ -43,11 +43,11 @@ const ErrorExample = (args) => (
 );
 const TextFieldExample = (args) => (
   <div className="flex-col">
-  <div className="flex justify-between"><span className="font-text text-surface-700">Text field description</span> <span className="font-icons-solid w-6 h-6 rounded-full bg-surface-500 text-surface-50">question_mark</span></div>
+  <div className="flex justify-between"><span className="font-text text-sm text-surface-700">Text field description</span> <span className="font-icons-solid w-4 h-4 rounded-full text-center bg-surface-500 text-xs text-surface-50">question_mark</span></div>
   <TextArea error  {...args}><TextArea.RightElement><span className="font-icons-solid text-surface-300 text-lg h-6 w-6 text-center">favorite</span></TextArea.RightElement>
   <TextArea.LeftElement><span className="font-icons-solid text-surface-300 text-lg h-6 w-6 text-center">people</span></TextArea.LeftElement>
   </TextArea>
-  <div className="flex justify-between text-sm"><span className="font-text text-error-500">Text help</span> <span className="font-text text-surface-400">0/100</span></div>
+  <div className="flex justify-between text-sm"><span className="font-text text-surface-500">Text help</span> <span className="font-text text-surface-400">0/100</span></div>
 </div>
 );
 
