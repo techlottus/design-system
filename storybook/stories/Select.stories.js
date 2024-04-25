3edaec6188ab8fb29e945e25cabc77fd56913945
Maria-cris <maria.delacruz@lottuseducation.com>
2024-04-25 13:15:42 -0600

    docs

--- storybook/stories/Select.stories.js
+++ storybook/stories/Select.stories.js
@@ -21,11 +21,11 @@ export default {
 
 const standar = (args) => {
   const people = [
-    { id: 1, name: 'Durward Reynolds' },
-    { id: 2, name: 'Kenton Towne' },
-    { id: 3, name: 'Therese Wunsch' },
-    { id: 4, name: 'Benedict Kessler' },
-    { id: 5, name: 'Katelyn Rohan' },
+    { id: 1, name: 'Durward Reynolds', unavailable: false },
+    { id: 2, name: 'Kenton Towne', unavailable: false },
+    { id: 3, name: 'Therese Wunsch', unavailable: false },
+    { id: 4, name: 'Benedict Kessler', unavailable: true },
+    { id: 5, name: 'Katelyn Rohan', unavailable: false },
   ]
   
     const [selectedPerson, setSelectedPerson] = useState(people[0])
