b7bc748cbbe49326242477679cc65e880048128e
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-05-07 11:14:20 -0600

    align select icon

--- storybook/components/SelectChip/SelectChip.tsx
+++ storybook/components/SelectChip/SelectChip.tsx
@@ -14,12 +14,11 @@ interface SelectChipType {
 const SelectChip: React.FC = (props: SelectChipType) => {
 
     const { iconActive, iconInactive, active = false, className = "", label } = props;
-
     const [activeChip, setActiveChip] = useState<boolean>(active)
 
     return (
-        <RadioGroup.Option value={activeChip} onClick={() => setActiveChip(!activeChip)} className={cn("flex bg-surface-100 h-9 cursor-pointer border-2 rounded py-1.5 pr-1.5", className)}>
-            <div className="!ms-2 !me-2 my-auto">
+        <RadioGroup.Option value={activeChip} onClick={() => setActiveChip(!activeChip)} className={cn("flex align-baseline bg-surface-100 h-9 cursor-pointer border-2 rounded py-1.5 pr-1.5", className)}>
+            <div className="!ms-2 !me-2 !my-auto">
                 {activeChip ? iconActive : iconInactive}
             </div>
             <span className="bg-surface-100 font-texts">{label}</span>
