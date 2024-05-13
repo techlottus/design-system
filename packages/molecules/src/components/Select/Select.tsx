70de3d62936d995f07c5d9f8e9985ea549d382c4
Maria-cris <maria.delacruz@lottuseducation.com>
2024-05-13 13:58:26 -0600

    core: truncate

--- packages/molecules/src/components/Select/Select.tsx
+++ packages/molecules/src/components/Select/Select.tsx
@@ -22,7 +22,7 @@ const Button = (props:ButtonType) =>{
      {["focus:border-surface-500 border-surface-200"]:!disabled && !error,
      ["focus:border-error-500 border-error-500"]:error ,
      ["text-surface-200 cursor-none"]:disabled && !error},className)}>
-      <div className={cn('py-1.5 px-2 align-middle cursor-text flex-nowrap overflow-hidden',{["text-error-500"]:error})}>{children}</div>
+      <div className={cn('py-1.5 px-2 align-middle cursor-text  overflow-hidden truncate',{["text-error-500"]:error})}>{children}</div>
       <span className={cn('order-last font-icons-solid text-lg text-center h-fit p-2 rounded-s-none rounded-e border-l',{['group-focus:border-surface-500 bg-surface-100']:!disabled && !error,['border-surface-200 bg-surface-200 text-surface-900 opacity-50']:disabled && !error,[" group-focus:border-error-500 border-error-500 bg-error-100 "]:error})}>
         expand_more
       </span>
