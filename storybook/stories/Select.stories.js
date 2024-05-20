9e4ae4a60435b5fd6b45b1aa6cf1f6bbd0dbb908
Maria-cris <maria.delacruz@lottuseducation.com>
2024-05-20 14:21:48 -0600

    docs

--- storybook/stories/Select.stories.js
+++ storybook/stories/Select.stories.js
@@ -29,17 +29,20 @@ const standar = (args) => {
   
     const [selectedPerson, setSelectedPerson] = useState({name:"Name"})
     const [open,setOpen]=useState(false)
-
+    const handleclick = ()=>{
+      if(open){setOpen(false)}
+      else {setOpen(true)}
+    }
 
     return (
       <div className="w-52">
       <Select value={selectedPerson} 
       onChange={setSelectedPerson} >
-        <Select.Button {...args}  >{open? "Name" : selectedPerson.name}</Select.Button>
+        <Select.Button {...args} onClick={handleclick} >{open? "Name" : selectedPerson.name}</Select.Button>
         <Select.Options static className={cn("h-40",{["hidden"]:!open})}>
           {people.map((person) => (
             <Select.Option
-              onClick={()=>setOpen(false)}
+            onClick={()=>setOpen(false)} 
               key={person.id}
               value={person}
               disabled={!args.disabled?person.unavailable : args.disabled}
