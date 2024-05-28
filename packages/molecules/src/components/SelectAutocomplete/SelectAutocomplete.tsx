e839ac5b10b14365aa224f9ddbd1408b296a9c06
Maria-cris <maria.delacruz@lottuseducation.com>
2024-05-28 11:05:37 -0600

    core: added button

--- packages/molecules/src/components/SelectAutocomplete/SelectAutocomplete.tsx
+++ packages/molecules/src/components/SelectAutocomplete/SelectAutocomplete.tsx
@@ -3,54 +3,61 @@ import { InputType, optionType, optionsType, selectAutoTypes } from '../Types/Se
 import cn from "classnames"
 
 
-const SelectAutocomplete: selectAutoTypes= (props:selectAutoTypes) =>{
-  const {value,onChange,children}=props
+const SelectAutocomplete: selectAutoTypes = (props: selectAutoTypes) => {
+  const { value, onChange, children } = props
   return (
     <Combobox value={value} onChange={onChange} >
-     {children}
+      {children}
     </Combobox>
   )
 }
 
 
-const Input = (props:InputType) =>{
-  const { className,disabled=false,error=false,onChange=()=>{},label="",rightElement=<></>,displayValue=""} =props;
-
+const Input = (props: InputType) => {
+  const { className, disabled = false, error = false, onChange = () => { }, label = "", rightElement = <></>, displayValue } = props;
   return (
 
     <div
-     className={cn("group border  rounded flex  h-10 w-full justify-between align-middle font-texts ",
-     {["focus:border-surface-500 border-surface-200 text-surface-700"]:!disabled && !error,
-     ["focus:border-error-500 border-error-500"]:error ,
-     ["text-surface-200 cursor-none"]:disabled && !error},className)}>
+      className={cn(" border  rounded flex  h-10 w-full justify-between align-middle font-texts ",
+        {
+          [" has-[:focus]:border-surface-500 has-[:active]:border-surface-500 border-surface-200 text-surface-700"]: !disabled && !error,
+          ["border-error-500"]: error,
+          ["text-surface-200 cursor-none"]: disabled && !error,
+        }, className)}>
 
-      <div className={cn('py-1.5 px-2 align-middle h-full cursor-none overflow-hidden truncate font-texts ',{["text-error-500"]:error})}>
+      <div className={cn('py-1.5 px-2 align-middle h-full cursor-none overflow-hidden truncate font-texts ', { ["text-error-500"]: error })}>
         <div className='flex'>
           {rightElement}
-          <Combobox.Input onChange={onChange} placeholder={label} displayValue={displayValue}/>
+          <Combobox.Input onChange={onChange} placeholder={label} displayValue={displayValue} className="focus:outline-none w-full h-full" />
 
-          </div>
-         </div>
-      <span className={cn('order-last font-icons-solid text-lg text-center h-full p-2 rounded-s-none rounded-e border-l cursor-pointer ',{['group-focus:border-surface-500 bg-surface-100']:!disabled && !error,['border-surface-200 bg-surface-200 text-surface-900 opacity-50']:disabled && !error,[" group-focus:border-error-500 border-error-500 bg-error-100 "]:error})}>
+        </div>
+      </div>
+      <Combobox.Button  className={cn('order-last font-icons-solid text-lg text-center h-full p-2 rounded-s-none rounded-e border-l cursor-pointer ',
+        {
+          ['focus:border-surface-500 active:border-surface-500 bg-surface-100']: !disabled && !error,
+          ['border-surface-200 bg-surface-200 text-surface-900 opacity-50']: disabled && !error,
+          ["border-error-500 bg-error-100 "]: error,
+        })} >
         expand_more
-      </span>
+      </Combobox.Button>
+
     </div>
   )
 }
 
-const Options = (props:optionsType)=> {
-  const {children, className="" , ...restProps}=props;
+const Options = (props: optionsType) => {
+  const { children, className = "", ...restProps } = props;
   return (
-    <Combobox.Options className={cn("rounded w-full shadow overflow-auto font-texts p-0 my-0",className)} {...restProps}>
-   {children}
+    <Combobox.Options className={cn("rounded w-full shadow overflow-auto font-texts p-0 my-0", className)} {...restProps}>
+      {children}
     </Combobox.Options>
   )
 }
 
-const Option = (props:optionType) =>{
-  const {className="", children,disabled=false, as='div',value,key=""} =props;
+const Option = (props: optionType) => {
+  const { className = "", children, disabled = false, as = 'div', value, key = "" } = props;
   return (
-    <Combobox.Option key={key}  className={cn("font-texts p-3 hover:bg-surface-100 cursor-default",{["text-surface-200"]:disabled,["text-surface-700"]:!disabled},className)} disabled={disabled} as={as} value={value} >
+    <Combobox.Option key={key} className={cn("font-texts p-3 hover:bg-surface-100 cursor-default", { ["text-surface-200"]: disabled, ["text-surface-700"]: !disabled }, className)} disabled={disabled} as={as} value={value} >
       {children}
     </Combobox.Option>
   )
@@ -60,6 +67,6 @@ SelectAutocomplete.Input = Input;
 SelectAutocomplete.Options = Options;
 SelectAutocomplete.Option = Option;
 
- 
+
 
 export default SelectAutocomplete;
--- storybook/stories/SelectAutocomplete.stories.js
+++ storybook/stories/SelectAutocomplete.stories.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import SelectAutocomplete from "../components/SelectAutocomplete";
+import mdx from "../components/SelectAutocomplete/SelectAutocomplete.mdx"
 export default {
   title: "MOLECULES/SelectAutocomplete",
   component: SelectAutocomplete,
   tags: ["autodocs"],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
   args: {
     disabled: false,
     error: false,
@@ -18,9 +24,24 @@ const Template = (args) => {
     { id: 3, name: 'Therese Wunsch' },
     { id: 4, name: 'Benedict Kessler' },
     { id: 5, name: 'Katelyn Rohan' },
+    { id: 6, name: 'Durward Reynolds 2' },
+    { id: 7, name: 'Kenton Towne 2' },
+    { id: 8, name: 'Therese Wunsch 2' },
+    { id: 9, name: 'Benedict Kessler 2' },
+    { id: 10, name: 'Katelyn Rohan 2' },
+    { id: 11, name: 'Durward Reynolds 3' },
+    { id: 12, name: 'Kenton Towne 3' },
+    { id: 13, name: 'Therese Wunsch 3' },
+    { id: 14, name: 'Benedict Kessler 3' },
+    { id: 15, name: 'Katelyn Rohan 3' },
+    { id: 16, name: 'Durward Reynolds 3' },
+    { id: 17, name: 'Kenton Towne 3' },
+    { id: 18, name: 'Therese Wunsch 3' },
+    { id: 19, name: 'Benedict Kessler 3' },
+    { id: 20, name: 'Katelyn Rohan 3' },
   ]
   
-  const [selectedPerson, setSelectedPerson] = useState(people[0])
+  const [selectedPerson, setSelectedPerson] = useState()
   const [query, setQuery] = useState('')
 
   const filteredPeople =
